785bc28b02be63800730a80529eac5a6907ba831
pavel-stastny <pavel.stastny@gmail.com>
2010-03-09 12:09:39 +0000

    refactoring

--- gwtViewers/src/cz/i/kramerius/gwtviewers/client/selections/SelectionListener.java
+++
@@ -1,8 +0,0 @@
-package cz.i.kramerius.gwtviewers.client.selections;
-
-import cz.i.kramerius.gwtviewers.client.panels.ImageMoveWrapper;
-
-public interface SelectionListener {
-
-	public void selectionChanged(ImageMoveWrapper oldW, ImageMoveWrapper newW);
-}
