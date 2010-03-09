3c382fa349670830781ae5b696039dfebed8aa94
pavel-stastny <pavel.stastny@gmail.com>
2010-03-09 12:07:48 +0000

    refactoring

--- gwtViewers/src/cz/i/kramerius/gwtviewers/client/panels/ConfigurationChanged.java
+++
@@ -1,8 +0,0 @@
-package cz.i.kramerius.gwtviewers.client.panels;
-
-public interface ConfigurationChanged {
-
-	public void onJumpChange(String to);
-	
-	public void onModuleStepChange(String step);
-}
