b430757a3b8b1da65e996e76cc78ca2bb194863d
pavel-stastny <pavel.stastny@gmail.com>
2010-03-24 06:19:34 +0000

    generating pdf

--- search/src/java/cz/incad/Kramerius/GeneratePDFServlet.java
+++ search/src/java/cz/incad/Kramerius/GeneratePDFServlet.java
@@ -7,6 +7,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Stack;
 import java.util.logging.Level;
 
 import javax.servlet.ServletConfig;
@@ -52,8 +53,9 @@ public class GeneratePDFServlet extends GuiceServlet {
 			    final List<String> pages = new ArrayList<String>();
 			    fedoraAccess.processRelsExt(relsExt, new RelsExtHandler() {
 					
+			    	
 					@Override
-					public void handle(Element elm, FedoraRelationship relation) {
+					public void handle(Element elm, FedoraRelationship relation,Stack<Element> processingStack) {
 						try {
 							if (relation.equals(FedoraRelationship.hasPage)) {
 								String pid = elm.getAttributeNS(RDF_NAMESPACE_URI, "resource");
@@ -61,7 +63,27 @@ public class GeneratePDFServlet extends GuiceServlet {
 								pidParse.disseminationURI();
 								String objectId = pidParse.getObjectId();
 								pages.add(0,objectId);
+								
+							} else if ((relation.equals(FedoraRelationship.hasIntCompPart)) || 
+										(relation.equals(FedoraRelationship.hasInternalPart)) || 
+										(relation.equals(FedoraRelationship.hasItem)) || 
+										(relation.equals(FedoraRelationship.hasUnit)) || 
+										(relation.equals(FedoraRelationship.hasVolume))) {
+								
+								try {
+								
+									String pid = elm.getAttributeNS(RDF_NAMESPACE_URI, "resource");
+									PIDParser pidParse = new PIDParser(pid);
+									pidParse.disseminationURI();
+									String objectId = pidParse.getObjectId();
+									Document childExts = fedoraAccess.getRelsExt(objectId);
+									processingStack.push(childExts.getDocumentElement());
+								
+								} catch (IOException e) {
+									LOGGER.log(Level.SEVERE, e.getMessage(), e);
+								}
 							}
+							
 						} catch (DOMException e) {
 							LOGGER.log(Level.SEVERE, e.getMessage(), e);
 							throw new RuntimeException(e);
@@ -75,8 +97,10 @@ public class GeneratePDFServlet extends GuiceServlet {
 						return true;
 					}
 				});
+			    
 			    // generovani pdf
-			    service.generatePDF(uuid, pages, resp.getOutputStream());
+			    //service.generatePDF(uuid, pages, resp.getOutputStream());
+			    service.generatePDFOutlined(uuid, pages, resp.getOutputStream());
 			}
 		} catch (Exception e) {
 			LOGGER.log(Level.SEVERE, e.getMessage(), e);
