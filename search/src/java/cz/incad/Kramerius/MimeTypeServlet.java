a6ff7500de7b78a41964782c56cfa6a704d039f6
pavel-stastny <pavel.stastny@gmail.com>
2010-03-25 14:03:44 +0000

    mimetype support

---
+++ search/src/java/cz/incad/Kramerius/MimeTypeServlet.java
@@ -0,0 +1,40 @@
+package cz.incad.Kramerius;
+
+import static cz.incad.utils.IKeys.UUID_PARAMETER;
+
+import java.io.IOException;
+import java.util.logging.Level;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.xpath.XPathExpressionException;
+
+import com.google.inject.Inject;
+
+import cz.incad.Kramerius.backend.guice.GuiceServlet;
+import cz.incad.kramerius.FedoraAccess;
+
+public class MimeTypeServlet extends GuiceServlet {
+
+	public static final java.util.logging.Logger LOGGER = java.util.logging.Logger
+			.getLogger(MimeTypeServlet.class.getName());
+	
+	@Inject
+	FedoraAccess fedoraAccess;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			String uuid = req.getParameter(UUID_PARAMETER);
+			if ((uuid != null) && (!uuid.equals(""))) {
+				String mimeType = this.fedoraAccess.getImageFULLMimeType(uuid);
+				resp.getWriter().println(mimeType);
+			}
+		} catch (XPathExpressionException e) {
+			LOGGER.log(Level.SEVERE, e.getMessage(), e);
+		}
+	}
+
+}
