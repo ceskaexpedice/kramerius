d2799b4cd262734fb46f99b909d3a811eeedfbb1
pavel-stastny <pavel.stastny@gmail.com>
2010-05-04 07:57:49 +0000

    changes in indexer

--- indexer/src/cz/incad/kramerius/indexer/Main.java
+++ indexer/src/cz/incad/kramerius/indexer/Main.java
@@ -5,33 +5,69 @@
 
 package cz.incad.kramerius.indexer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 
+import cz.incad.kramerius.utils.IOUtils;
+
 /**
- *
+ * 
  * @author Administrator
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        try {
-System.out.println(Arrays.asList(args));
-            ProgramArguments arguments = new ProgramArguments();
-            if (!arguments.parse(args)) {
-                System.out.println("Program arguments are invalid");
-            }
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+		try {
+			System.out.println(Arrays.asList(args));
+			ProgramArguments arguments = new ProgramArguments();
+			if (!arguments.parse(args)) {
+				System.out.println("Program arguments are invalid");
+			}
 
-            Indexer indexer = new Indexer(arguments);
-            indexer.run();
-            
+			
+			checkFileOrCreateNew(arguments.log4jFile, "res/log4j.properties");
+			checkFileOrCreateNew(arguments.configFile,"res/config.properties");
 
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            System.out.println(ex);
-        }
-    }
+			
+			Indexer indexer = new Indexer(arguments);
+			indexer.run();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println(ex);
+		}
+	}
+
+
+	private static void checkFileOrCreateNew(String log4jFile, String resPath) throws IOException {
+		File file = new File(log4jFile);
+		if (!file.exists()) {
+			file.getParentFile().mkdirs();
+			file.createNewFile();
+			InputStream resStream = null;
+			try {
+				FileOutputStream fos = new FileOutputStream(file);
+				resStream = Main.class.getResourceAsStream(resPath);
+				IOUtils.copyStreams(resStream, fos);
+			} finally {
+				if (resStream != null) {
+					resStream.close();
+				}
+			}
+		}
+	}
+
+
+	private static void createParentDirs(File file) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
