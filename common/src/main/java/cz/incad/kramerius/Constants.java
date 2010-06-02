92ddae2f8e1b335ffbd952fcbc505438331eb269
pavel-stastny <pavel.stastny@gmail.com>
2010-06-02 13:02:31 +0000

    i18n

--- common/src/main/java/cz/incad/kramerius/Constants.java
+++ common/src/main/java/cz/incad/kramerius/Constants.java
@@ -5,9 +5,6 @@ import java.io.File;
 public class Constants {
 
 	public static final String WORKING_DIR=System.getProperty("user.home")+File.separator+".kramerius4";
-//	public static final String WORKING_DB_JDBC_URL="jdbc:derby:"+WORKING_DIR+"/kramerius4.db;create=true;user=kramerius4;password=kramerius4";
-	public static final String DERBY_DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
-	public static final String WORKING_DB_JDBC_URL="jdbc:derby:"+WORKING_DIR+"/kramerius4.db;create=true;user=kramerius4;password=kramerius4";
 
 	static {
 		File dir = new File(WORKING_DIR);
--- common/src/main/java/cz/incad/kramerius/FedoraModels.java
+++ common/src/main/java/cz/incad/kramerius/FedoraModels.java
@@ -10,5 +10,4 @@ public enum FedoraModels {
 	periodicalitem,
 	periodicalvolume,
 	
-
 }
