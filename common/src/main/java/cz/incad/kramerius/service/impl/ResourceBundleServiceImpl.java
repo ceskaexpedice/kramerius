04ec988d9b565f520294f29d6f6e565bd7f1e97a
pavel-stastny <pavel.stastny@gmail.com>
2010-06-02 13:07:39 +0000

    i18n

---
+++ common/src/main/java/cz/incad/kramerius/service/impl/ResourceBundleServiceImpl.java
@@ -0,0 +1,100 @@
+package cz.incad.kramerius.service.impl;
+
+import static cz.incad.kramerius.utils.IOUtils.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Vector;
+import java.util.logging.Level;
+
+import cz.incad.kramerius.Constants;
+import cz.incad.kramerius.service.ResourceBundleService;
+
+public class ResourceBundleServiceImpl implements ResourceBundleService {
+
+
+	
+	@Override
+	public File bundlesFolder() {
+		String dirName = Constants.WORKING_DIR + File.separator + "bundles";
+		File dir = new File(dirName);
+		if (!dir.exists()) { dir.mkdirs(); }
+		return dir;
+	}
+
+
+	@Override
+	public ResourceBundle getResourceBundle(String name, Locale locale) throws IOException {
+		File resourcesDir = bundlesFolder();
+		if ((resourcesDir.listFiles() == null) || (resourcesDir.listFiles().length == 0)) {
+			copyDefault();
+		}
+		return ResourceBundle.getBundle(name, locale, new ResourceClassLoader(resourcesDir));
+	}
+
+
+	private void copyDefault() throws IOException {
+		String[] defaults = 
+		{"base.properties",
+		"base_en.properties",
+		"base_cs.properties"};
+		for (String base : defaults) {
+			InputStream is = null;
+			OutputStream os = null;
+			try {
+				is = this.getClass().getResourceAsStream("res/"+base);
+				os = new FileOutputStream(new File(bundlesFolder(),base));
+				copyStreams(is, os);
+			} finally {
+				if (os != null) is.close();
+				if (is != null) is.close();
+			}
+		}
+	}
+	
+	
+	public static class ResourceClassLoader extends ClassLoader {
+
+		public static final java.util.logging.Logger LOGGER = java.util.logging.Logger
+				.getLogger(ResourceBundleServiceImpl.ResourceClassLoader.class
+						.getName());
+		
+		private File folder;
+
+		public ResourceClassLoader(File folder) {
+			super();
+			this.folder = folder;
+		}
+
+		@Override
+		protected URL findResource(String name) {
+			try {
+				File file = new File(this.folder, name);
+				if (file.exists()) {
+					return file.toURI().toURL();
+				} else return null;
+			} catch (MalformedURLException e) {
+				LOGGER.log(Level.SEVERE, e.getMessage(), e);
+				return null;
+			}
+		}
+
+		@Override
+		protected Enumeration<URL> findResources(String name) throws IOException {
+			URL url = findResource(name);
+			Enumeration<URL> elements = new Vector(Arrays.asList(url)).elements();
+			return elements;
+		}
+	}
+
+}
---
+++ common/src/main/java/cz/incad/kramerius/service/impl/TextsServiceImpl.java
@@ -0,0 +1,120 @@
+package cz.incad.kramerius.service.impl;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Locale;
+
+import cz.incad.kramerius.Constants;
+import cz.incad.kramerius.service.TextsService;
+import cz.incad.kramerius.utils.IOUtils;
+
+/**
+ *
+ * @author Administrator
+ */
+public class TextsServiceImpl implements TextsService {
+	
+	public static final java.util.logging.Logger LOGGER = java.util.logging.Logger
+			.getLogger(TextsServiceImpl.class.getName());
+	
+	private static HashMap<String, Locale> LOCALES = new HashMap<String, Locale>() {{
+    	put("en",Locale.ENGLISH);
+    	put("cs",new Locale("cs", "cz"));
+    }};
+
+	
+    public  String getText(String name, Locale locale) throws IOException {
+        StringBuffer result = new StringBuffer();
+        if (locale == null) {
+        	locale = Locale.getDefault();
+        }
+        File textFile = textFile(textsFolder(), name, locale);
+        if (!textFile.exists()) {
+        	textFile = textFileWithoutLocale(textsFolder(), name);
+        	LOGGER.info("using textFile without locale '"+textFile+"'");
+        }
+        if ((!textFile.exists())  || (!textFile.canRead())) throw new IOException("cannot read from file '"+name+"'");
+        String retVal = IOUtils.readAsString(new FileInputStream(textFile), Charset.forName("UTF-8"), true);
+        return retVal;
+    }
+
+
+    private File textFileWithoutLocale(File textsDir, String name) {
+		File textFile = new File(textsDir, name );
+		return textFile;
+	}
+
+
+	public  void writeText(String name, Locale locale, String text) throws  IOException {
+        if (text == null) {
+            System.out.println("invalid text");
+        } else {
+        	if (locale == null) {
+        		locale = Locale.getDefault();
+        	}
+            File file = textFile(textsFolder(), name, locale);
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8"));
+            out.write(text);
+            out.close();
+        }
+    }
+
+    
+	@Override
+	public Locale findLocale(String languageAcronym) {
+		Locale locale = LOCALES.get(languageAcronym);
+		if (locale == null) {
+			Locale[] aLocales = Locale.getAvailableLocales();
+			for (Locale iterate : aLocales) {
+				if (iterate.getLanguage().equals(languageAcronym)) {
+					locale = iterate;
+					break;
+				}
+			}
+		}
+		return locale;
+	}
+    
+
+
+	
+	
+	@Override
+	public File textsFolder() {
+		String dirName = Constants.WORKING_DIR + File.separator + "texts";
+		File dir = new File(dirName);
+		if (!dir.exists()) { dir.mkdirs(); }
+		return dir;
+	}
+
+
+	private File textFile(File textsDir, String name, Locale locale) {
+		File textFile = new File(textsDir, name + "_" + locale.getCountry()+"_"+locale.getLanguage());
+		return textFile;
+	}
+
+	public static void main(String[] args) {
+		Locale[] locales = Locale.getAvailableLocales();
+		for (Locale locale : locales) {
+			System.out.println(locale);
+		}
+		System.out.println(new TextsServiceImpl().findLocale("cs").getDisplayCountry());
+	}
+}
