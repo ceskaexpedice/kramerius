aff27afcb6efba8941fc2879bd1cfad1ac68f28b
pavel-stastny <pavel.stastny@gmail.com>
2010-06-16 10:09:57 +0000

    find bugs

--- common/src/main/java/cz/incad/kramerius/utils/IOUtils.java
+++ common/src/main/java/cz/incad/kramerius/utils/IOUtils.java
@@ -52,31 +52,6 @@ public class IOUtils {
 		}
 	}
 	
-	public static void copyFile(File src, File dst) throws IOException {
-		LOGGER.info("Copying file '"+src.getAbsolutePath()+"' to '"+dst.getAbsolutePath()+"'");
-		FileChannel in = null;
-		FileChannel out = null;
-		try {
-			FileInputStream fis = new FileInputStream(src);
-			in = fis.getChannel();
-			FileOutputStream fos = new FileOutputStream(dst);
-			out = fos.getChannel();
-		    out.transferFrom(in, 0, in.size());
-		} finally  {
-			try {
-				if (in != null) in.close();
-			} catch (IOException e) {
-				LOGGER.log(Level.SEVERE, e.getMessage(), e);
-			}
-			if (out != null) {
-				try {
-					out.close();
-				} catch (IOException e) {
-					LOGGER.log(Level.SEVERE, e.getMessage(), e);
-				}
-			}
-		}
-	}
 
 	public static String readAsString(InputStream is, Charset charset, boolean closeInput) throws IOException {
 		try {
