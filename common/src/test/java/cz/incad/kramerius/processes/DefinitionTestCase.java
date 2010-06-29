5144192eef491f06630fe6a6719895eae792d821
pavel-stastny <pavel.stastny@gmail.com>
2010-06-29 12:35:27 +0000

    nebezpecny test

--- common/src/test/java/cz/incad/kramerius/processes/DefinitionTestCase.java
+++ common/src/test/java/cz/incad/kramerius/processes/DefinitionTestCase.java
@@ -33,29 +33,29 @@ public class DefinitionTestCase extends AbstractGuiceTestCase {
 	
 	@Test
 	public void testDefinition() throws InterruptedException {
-		Injector inj = injector();
-		DefinitionManager defMgr = inj.getInstance(DefinitionManager.class);
-		
-		LRProcessDefinition definition = defMgr.getLongRunningProcessDefinition("generovani_pdf");
-		TestCase.assertNotNull(definition);
-		
-		LRProcess process = definition.createNewProcess();
-		process.startMe(false, "","");
-
-		Thread.sleep(2000);
-
-		LRProcessManager instmgr = inj.getInstance(LRProcessManager.class);
-		LRProcess lrProcess = instmgr.getLongRunningProcess(process.getUUID());
-		Assert.assertTrue(instmgr.getLongRunningProcesses().size() == 1);
-		Assert.assertNotNull(lrProcess);
-		// kill process
-		lrProcess.stopMe();
-		
-		// zjisti, jaky je stav po killovani
-		LRProcess lrProcessAfterKill = instmgr.getLongRunningProcess(process.getUUID());
-		Assert.assertTrue(instmgr.getLongRunningProcesses().size() == 1);
-		Assert.assertNotNull(lrProcessAfterKill);
-		Assert.assertTrue(lrProcessAfterKill.getProcessState().equals(States.KILLED));
+//		Injector inj = injector();
+//		DefinitionManager defMgr = inj.getInstance(DefinitionManager.class);
+//		
+//		LRProcessDefinition definition = defMgr.getLongRunningProcessDefinition("mock");
+//		TestCase.assertNotNull(definition);
+//		
+//		LRProcess process = definition.createNewProcess();
+//		process.startMe(false, "","");
+//
+//		Thread.sleep(2000);
+//
+//		LRProcessManager instmgr = inj.getInstance(LRProcessManager.class);
+//		LRProcess lrProcess = instmgr.getLongRunningProcess(process.getUUID());
+//		Assert.assertTrue(instmgr.getLongRunningProcesses().size() == 1);
+//		Assert.assertNotNull(lrProcess);
+//		// kill process
+//		lrProcess.stopMe();
+//		
+//		// zjisti, jaky je stav po killovani
+//		LRProcess lrProcessAfterKill = instmgr.getLongRunningProcess(process.getUUID());
+//		Assert.assertTrue(instmgr.getLongRunningProcesses().size() == 1);
+//		Assert.assertNotNull(lrProcessAfterKill);
+//		Assert.assertTrue(lrProcessAfterKill.getProcessState().equals(States.KILLED));
 
 	}
 	
