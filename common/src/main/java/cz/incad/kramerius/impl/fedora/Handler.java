5778807d1cd1ff0d8aeff0e61d2d7073aa710508
pavel-stastny <pavel.stastny@gmail.com>
2010-07-13 09:33:57 +0000

    url repair

---
+++ common/src/main/java/cz/incad/kramerius/impl/fedora/Handler.java
@@ -0,0 +1,29 @@
+package cz.incad.kramerius.impl.fedora;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLStreamHandler;
+
+import com.google.inject.Inject;
+
+import cz.incad.kramerius.FedoraAccess;
+
+
+public class Handler extends URLStreamHandler {
+
+	private FedoraAccess fedoraAccess;
+	
+	@Inject
+	public Handler(FedoraAccess fedoraAccess) {
+		super();
+		this.fedoraAccess = fedoraAccess;
+	}
+
+
+	@Override
+	protected URLConnection openConnection(URL u) throws IOException {
+		return new FedoraURLConnection(u, fedoraAccess);
+	}
+
+}
