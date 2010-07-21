62055c18ff6d968f89b9994816fe1fa9a0fb9be2
pavel-stastny <pavel.stastny@gmail.com>
2010-07-21 06:31:58 +0000

    comment

--- common/src/main/java/cz/incad/kramerius/service/TextsService.java
+++ common/src/main/java/cz/incad/kramerius/service/TextsService.java
@@ -4,13 +4,37 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Locale;
 
+/**
+ * This service provides localized texts.  
+ * It is similar like resource bundle but intended to longer texts.
+ * @author pavels
+ */
 public interface TextsService {
-
+	
+	/**
+	 * Return localized texts
+	 * @param name Name of text
+	 * @param locale Locale
+	 * @return
+	 * @throws IOException
+	 */
 	public String getText(String name, Locale locale) throws IOException;
 
+	/**
+	 * Save new text
+	 * @param name name of text
+	 * @param locale locale
+	 * @param text new text
+	 * @throws IOException
+	 */
 	public void writeText(String name, Locale locale, String text) throws IOException;
 
+	/**
+	 * Folder for user defined texts
+	 * @return
+	 */
 	public File textsFolder();
+	
 	
 	public Locale findLocale(String languageAcronym);
 }
