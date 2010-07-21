390ba695e1c27f47542816e9fb80bc86987a0011
pavel-stastny <pavel.stastny@gmail.com>
2010-07-21 06:27:52 +0000

    comments

--- common/src/main/java/cz/incad/kramerius/service/ResourceBundleService.java
+++ common/src/main/java/cz/incad/kramerius/service/ResourceBundleService.java
@@ -5,9 +5,24 @@ import java.io.IOException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+/**
+ * This service provides resourceBundle
+ * @author pavels
+ */
 public interface ResourceBundleService {
 	
+	/**
+	 * Returns ResourceBundle 
+	 * @param name name of the bundle 
+	 * @param locale Bundle's locale 
+	 * @return
+	 * @throws IOException
+	 */
 	public ResourceBundle getResourceBundle(String name, Locale locale) throws IOException;
 
+	/**
+	 * Returns folder for user defined resource bundles
+	 * @return
+	 */
 	public File bundlesFolder();
 }
