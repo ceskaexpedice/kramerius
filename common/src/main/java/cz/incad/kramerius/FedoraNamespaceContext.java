1e52402364c71b22c98e20f1127e913cbcc1e8e9
pokorsky <pokorsky@gmail.com>
2010-09-09 16:27:09 +0000

    optimization

--- common/src/main/java/cz/incad/kramerius/FedoraNamespaceContext.java
+++ common/src/main/java/cz/incad/kramerius/FedoraNamespaceContext.java
@@ -1,9 +1,8 @@
 package cz.incad.kramerius;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
+import java.util.Collections;
+import java.util.IdentityHashMap;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -13,45 +12,38 @@ import javax.xml.namespace.NamespaceContext;
 
 public class FedoraNamespaceContext implements NamespaceContext {
 
-    static Map<String, String> NAMESPACES_DEFAULT_MAPPING = new HashMap<String, String>();
+    private static final Map<String, String> MAP_PREFIX2URI = new IdentityHashMap<String, String>();
+    private static final Map<String, String> MAP_URI2PREFIX = new IdentityHashMap<String, String>();
 
     static {
-        NAMESPACES_DEFAULT_MAPPING.put("mods", BIBILO_MODS_URI);
-        NAMESPACES_DEFAULT_MAPPING.put("dc", DC_NAMESPACE_URI);
-        NAMESPACES_DEFAULT_MAPPING.put("fedora-models", FEDORA_MODELS_URI);
-        NAMESPACES_DEFAULT_MAPPING.put("kramerius", KRAMERIUS_URI);
-        NAMESPACES_DEFAULT_MAPPING.put("kramerius", KRAMERIUS_URI);
-        NAMESPACES_DEFAULT_MAPPING.put("rdf", RDF_NAMESPACE_URI);
+        MAP_PREFIX2URI.put("mods", BIBILO_MODS_URI);
+        MAP_PREFIX2URI.put("dc", DC_NAMESPACE_URI);
+        MAP_PREFIX2URI.put("fedora-models", FEDORA_MODELS_URI);
+        MAP_PREFIX2URI.put("kramerius", KRAMERIUS_URI);
+        MAP_PREFIX2URI.put("rdf", RDF_NAMESPACE_URI);
+
+        for (Map.Entry<String, String> entry : MAP_PREFIX2URI.entrySet()) {
+            MAP_URI2PREFIX.put(entry.getValue(), entry.getKey());
+        }
     }
 
     @Override
     public String getNamespaceURI(String arg0) {
-        return NAMESPACES_DEFAULT_MAPPING.get(arg0);
+        return MAP_PREFIX2URI.get(arg0.intern());
     }
 
     @Override
     public String getPrefix(String arg0) {
-        return getPrefixInternal(arg0);
-    }
-
-    private String getPrefixInternal(String uri) {
-        Collection<String> keys = NAMESPACES_DEFAULT_MAPPING.keySet();
-        for (String key : keys) {
-            String val = NAMESPACES_DEFAULT_MAPPING.get(key);
-            if (val.equals(uri)) {
-                return key;
-            }
-        }
-        return null;
+        return MAP_URI2PREFIX.get(arg0.intern());
     }
 
     @Override
     public Iterator getPrefixes(String arg0) {
-        String prefixInternal = getPrefixInternal(arg0);
+        String prefixInternal = MAP_URI2PREFIX.get(arg0.intern());
         if (prefixInternal != null) {
             return Arrays.asList(prefixInternal).iterator();
         } else {
-            return new ArrayList().iterator();
+            return Collections.emptyList().iterator();
         }
     }
 }
