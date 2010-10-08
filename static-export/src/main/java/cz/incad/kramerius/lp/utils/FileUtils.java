900967d4da6fc544a7544f2b3300fbccf5dd4ba7
pavel-stastny <pavel.stastny@gmail.com>
2010-10-08 07:26:09 +0000

    move FileUtils

---
+++ static-export/src/main/java/cz/incad/kramerius/lp/utils/FileUtils.java
@@ -0,0 +1,37 @@
+package cz.incad.kramerius.lp.utils;
+
+import java.io.File;
+import java.util.Stack;
+
+public class FileUtils {
+
+	
+	public static void deleteRecursive(File folder) {
+		File[] listFiles = folder.listFiles();
+		if (listFiles != null) {
+			for (File file : listFiles) {
+				if (file.isFile()) {
+					file.delete();
+				} else {
+					deleteRecursive(file);
+					file.delete();
+				}
+			}
+		}
+	}
+
+	public static void deleteTMPFolder() {
+		String property = System.getProperty("user.dir");
+		File ffolder = new File(property+File.separator+"tmp");
+		deleteRecursive(ffolder);
+		ffolder.delete();
+	}
+
+	public static File createTMPFolder() {
+		String property = System.getProperty("user.dir");
+		File ffolder = new File(property+File.separator+"tmp");
+		ffolder.mkdirs();
+		return ffolder;
+	}
+
+}
