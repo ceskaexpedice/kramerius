8ab7235618fc7f922cc5501fcfb6300b180aa625
pavel-stastny <pavel.stastny@gmail.com>
2010-11-01 21:11:24 +0000

    some comments

--- common/src/main/java/cz/incad/kramerius/processes/LRProcessManager.java
+++ common/src/main/java/cz/incad/kramerius/processes/LRProcessManager.java
@@ -22,17 +22,72 @@ public interface LRProcessManager {
 	 * @return
 	 */
 	public LRProcess getLongRunningProcess(String uuid);
+
+	/**
+	 * Return all lr processes
+	 * @return
+	 */
 	public List<LRProcess> getLongRunningProcesses();
+
+	/**
+	 * Retrun planned processes 
+	 * @param howMany How many processes should be returned
+	 * @return
+	 */
 	public List<LRProcess> getPlannedProcess(int howMany);
+	
+	/**
+	 * Return planned processes for given rules
+	 * @param ordering Ordering column
+	 * @param typeOfOrdering Type of ordering (ASC, DESC)
+	 * @param offset offset
+	 * @return
+	 */
 	public List<LRProcess> getLongRunningProcesses(LRProcessOrdering ordering,TypeOfOrdering typeOfOrdering, LRProcessOffset offset);
+	
+	/**
+	 * Returns all processes for given state
+	 * @param state
+	 * @return
+	 */
 	public List<LRProcess> getLongRunningProcesses(States state);
 	
+	
+	/**
+	 * Returns number of running processes
+	 * @return
+	 */
 	public int getNumberOfLongRunningProcesses();
 	
+	/**
+	 * Update state 
+	 * @param lrProcess
+	 */
 	public void updateLongRunningProcessState(LRProcess lrProcess);
+	
+	/**
+	 * Update name
+	 * @param lrProcess
+	 */
 	public void updateLongRunningProcessName(LRProcess lrProcess);
+	
+	/**
+	 * Update PID
+	 * @param lrProcess
+	 */
 	public void updateLongRunningProcessPID(LRProcess lrProcess);
+	
+	/**
+	 * Update started date
+	 * @param lrProcess
+	 */
 	public void updateLongRunningProcessStartedDate(LRProcess lrProcess);
-
+	
+	/**
+	 * Delete process
+	 * @param uuid
+	 */
+	public void deleteLongRunningProcess(LRProcess lrProcess);
+	
 	public Lock getSynchronizingLock();
 }
