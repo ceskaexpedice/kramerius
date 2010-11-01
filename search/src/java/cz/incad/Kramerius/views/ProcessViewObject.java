55eec8cf5fac3d66c9aa9bd6297a2dae3ebaa99f
pavel-stastny <pavel.stastny@gmail.com>
2010-11-01 21:19:05 +0000

    delete process feature

--- search/src/java/cz/incad/Kramerius/views/ProcessViewObject.java
+++ search/src/java/cz/incad/Kramerius/views/ProcessViewObject.java
@@ -22,119 +22,139 @@ import cz.incad.kramerius.utils.conf.KConfiguration;
 
 public class ProcessViewObject {
 
-	public static final java.util.logging.Logger LOGGER = java.util.logging.Logger
-			.getLogger(ProcessViewObject.class.getName());
-	
-	public static final SimpleDateFormat FORMAT = new SimpleDateFormat("yyyy/dd/MM - HH:mm:ss");
-	
-	private LRProcess lrProcess;
-	private LRProcessDefinition definition;
-	private LRProcessOrdering ordering;
-	private LRProcessOffset offset;
-	private TypeOfOrdering typeOfOrdering;
-	private String lrUrl;
-	private ResourceBundleService bundleService;
-	private Locale locale;
-	
-	public ProcessViewObject(LRProcess lrProcess, LRProcessDefinition definition, LRProcessOrdering ordering, LRProcessOffset offset, TypeOfOrdering typeOfOrdering, String lrUrl, ResourceBundleService service, Locale locale) {
-		super();
-		this.lrProcess = lrProcess;
-		this.ordering = ordering;
-		this.offset = offset;
-		this.typeOfOrdering = typeOfOrdering;
-		this.definition = definition;
-		this.lrUrl = lrUrl;
-		this.bundleService = service;
-		this.locale = locale;
-	}
+    public static final java.util.logging.Logger LOGGER = java.util.logging.Logger.getLogger(ProcessViewObject.class.getName());
 
-	public String getPid() {
-		return lrProcess.getPid();
-	}
+    public static final SimpleDateFormat FORMAT = new SimpleDateFormat("yyyy/dd/MM - HH:mm:ss");
 
-	public String getName() {
-		try {
-			String unnamed = bundleService.getResourceBundle("labels", locale).getString("administrator.processes.unnamedprocess");
-			if (lrProcess.getProcessName() == null) {
-				return unnamed+" <br> <span style='font-size:80%; font-style:italic'>"+lrProcess.getDescription()+"</span>";
-			} else {
-			    return lrProcess.getProcessName()+" <br> <span style='font-size:80%; font-style:italic'>"+lrProcess.getDescription()+"</span>";
-			}
-		} catch (IOException e) {
-			LOGGER.log(Level.SEVERE, e.getMessage(), e);
-			return "";
-		}
-	}
-	
-	public String getProcessName() {
-		return getName();
-//		if (this.definition.getProcessOutputURL() != null) {
-//			return "<a href=\""+this.definition.getProcessOutputURL()+"?uuid="+this.lrProcess.getUUID()+"\" target=\"_blank\">"+getName()+"</a>";
-//		} else {
-//			return "<a href=\"dialogs/_processes_logs.jsp?uuid="+this.lrProcess.getUUID()+"\" target=\"_blank\">"+getName()+"</a>";
-//		}
-	}
+    private LRProcess lrProcess;
+    private LRProcessDefinition definition;
+    private LRProcessOrdering ordering;
+    private LRProcessOffset offset;
+    private TypeOfOrdering typeOfOrdering;
+    private String lrUrl;
+    private ResourceBundleService bundleService;
+    private Locale locale;
 
-	public String getProcessState() {
-		return lrProcess.getProcessState().getVal()+" "+lrProcess.getProcessState().name();
-	}
+    public ProcessViewObject(LRProcess lrProcess, LRProcessDefinition definition, LRProcessOrdering ordering, LRProcessOffset offset, TypeOfOrdering typeOfOrdering, String lrUrl, ResourceBundleService service, Locale locale) {
+        super();
+        this.lrProcess = lrProcess;
+        this.ordering = ordering;
+        this.offset = offset;
+        this.typeOfOrdering = typeOfOrdering;
+        this.definition = definition;
+        this.lrUrl = lrUrl;
+        this.bundleService = service;
+        this.locale = locale;
+    }
 
-	public String getStart() {
-		Date date = new Date(lrProcess.getStartTime());
+    public String getPid() {
+        return lrProcess.getPid();
+    }
+
+    public String getName() {
+        try {
+            String unnamed = bundleService.getResourceBundle("labels", locale).getString("administrator.processes.unnamedprocess");
+            if (lrProcess.getProcessName() == null) {
+                return unnamed + " <br> <span style='font-size:80%; font-style:italic'>" + lrProcess.getDescription() + "</span>";
+            } else {
+                return lrProcess.getProcessName() + " <br> <span style='font-size:80%; font-style:italic'>" + lrProcess.getDescription() + "</span>";
+            }
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            return "";
+        }
+    }
+
+    public String getProcessName() {
+        return getName();
+        // if (this.definition.getProcessOutputURL() != null) {
+        // return
+        // "<a href=\""+this.definition.getProcessOutputURL()+"?uuid="+this.lrProcess.getUUID()+"\" target=\"_blank\">"+getName()+"</a>";
+        // } else {
+        // return
+        // "<a href=\"dialogs/_processes_logs.jsp?uuid="+this.lrProcess.getUUID()+"\" target=\"_blank\">"+getName()+"</a>";
+        // }
+    }
+
+    public String getProcessState() {
+        return lrProcess.getProcessState().getVal() + " " + lrProcess.getProcessState().name();
+    }
+
+    public String getStart() {
+        Date date = new Date(lrProcess.getStartTime());
         return FORMAT.format(date);
-	}
+    }
 
-	public String getPlanned() {
+    public String getPlanned() {
         Date date = new Date(lrProcess.getPlannedTime());
         return FORMAT.format(date);
-	}
-	
-	//function killAndRefresh(url,ordering, offset, size, type) {
+    }
 
-	public String getKillURL() {
-		try {
-			if ((this.lrProcess.getProcessState().equals(States.RUNNING)) || 
-			(this.lrProcess.getProcessState().equals(States.PLANNED)))  {
-				String url = lrUrl+"?action=stop&uuid="+this.lrProcess.getUUID();
-				String renderedAHREF = "<a href=\"javascript:killAndRefresh('"+url+"','"+this.ordering.name()+"',"+this.offset.getOffset()+","+this.offset.getSize()+",'"+this.typeOfOrdering.name()+"');\">"+bundleService.getResourceBundle("labels", locale).getString("administrator.processes.kill.process")+"</a>";
-				if (!this.definition.getActions().isEmpty()) {
-					renderedAHREF += " || ";
-				}
-				return renderedAHREF;
-			} else {
-				return "";
-			}
-		} catch (IOException e) {
-			LOGGER.log(Level.SEVERE, e.getMessage(), e);
-			return "";
-		}
-	}
+    // function killAndRefresh(url,ordering, offset, size, type) {
 
-	public String getActionsURLs() {
-		StringBuilder builder =  new StringBuilder();
-		List<LRDefinitionAction> actions = this.definition.getActions();
-		for (int i = 0,ll=actions.size(); i < ll; i++) {
-			LRDefinitionAction action = actions.get(i);
-			builder.append(getActionAHREF(action));
-			if (i != ll-1) {
-				builder.append(" || ");
-			}
-		}
-		return builder.toString();
-	}
-	
-	private String getActionAHREF(LRDefinitionAction act) {
-		try {
-			String bundleKey = act.getResourceBundleKey();
-			if (bundleKey != null) {
-				return "<a href=\""+act.getActionURL()+"?uuid="+this.lrProcess.getUUID()+"\" target=\"_blank\">"+ bundleService.getResourceBundle("labels", locale).getString(bundleKey) +"</a>";
-			} else {
-				LOGGER.info(" action '"+act.getName()+"' has no bundle key");
-				return "<a href=\""+act.getActionURL()+"?uuid="+this.lrProcess.getUUID()+"\" target=\"_blank\">"+ act.getName() +"</a>";
-			}
-		} catch (IOException e) {
-			LOGGER.log(Level.SEVERE, e.getMessage(), e);
-			return "";
-		}
-	}
+    public String getKillURL() {
+        try {
+            if ((this.lrProcess.getProcessState().equals(States.RUNNING)) || (this.lrProcess.getProcessState().equals(States.PLANNED))) {
+                String url = lrUrl + "?action=stop&uuid=" + this.lrProcess.getUUID();
+                String renderedAHREF = "<a href=\"javascript:doActionAndRefresh('" + url + "','" + this.ordering.name() + "'," + this.offset.getOffset() + "," + this.offset.getSize() + ",'" + this.typeOfOrdering.name() + "');\">"
+                        + bundleService.getResourceBundle("labels", locale).getString("administrator.processes.kill.process") + "</a>";
+                if (!this.definition.getActions().isEmpty()) {
+                    renderedAHREF += " || ";
+                }
+                return renderedAHREF;
+            } else {
+                return "";
+            }
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            return "";
+        }
+    }
+
+    public String getDeleteURL() {
+        try {
+            if ((this.lrProcess.getProcessState().equals(States.FINISHED)) || (this.lrProcess.getProcessState().equals(States.KILLED)) || (this.lrProcess.getProcessState().equals(States.FAILED))) {
+                String url = lrUrl + "?action=delete&uuid=" + this.lrProcess.getUUID();
+                String renderedAHREF = "<a href=\"javascript:doActionAndRefresh('" + url + "','" + this.ordering.name() + "'," + this.offset.getOffset() + "," + this.offset.getSize() + ",'" + this.typeOfOrdering.name() + "');\">"
+                        + bundleService.getResourceBundle("labels", locale).getString("administrator.processes.delete.process") + "</a>";
+                if (!this.definition.getActions().isEmpty()) {
+                    renderedAHREF = " || "+ renderedAHREF;
+                }
+                return renderedAHREF;
+            } else {
+                return "";
+            }
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            return "";
+        }
+    }
+
+    public String getActionsURLs() {
+        StringBuilder builder = new StringBuilder();
+        List<LRDefinitionAction> actions = this.definition.getActions();
+        for (int i = 0, ll = actions.size(); i < ll; i++) {
+            LRDefinitionAction action = actions.get(i);
+            builder.append(getActionAHREF(action));
+            if (i != ll - 1) {
+                builder.append(" || ");
+            }
+        }
+        return builder.toString();
+    }
+
+    private String getActionAHREF(LRDefinitionAction act) {
+        try {
+            String bundleKey = act.getResourceBundleKey();
+            if (bundleKey != null) {
+                return "<a href=\"" + act.getActionURL() + "?uuid=" + this.lrProcess.getUUID() + "\" target=\"_blank\">" + bundleService.getResourceBundle("labels", locale).getString(bundleKey) + "</a>";
+            } else {
+                LOGGER.info(" action '" + act.getName() + "' has no bundle key");
+                return "<a href=\"" + act.getActionURL() + "?uuid=" + this.lrProcess.getUUID() + "\" target=\"_blank\">" + act.getName() + "</a>";
+            }
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            return "";
+        }
+    }
 }
