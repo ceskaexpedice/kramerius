8e712e91937b44c4e2acda1994c65a706d6725ce
pavel-stastny <pavel.stastny@gmail.com>
2010-11-01 21:11:39 +0000

    some comments

--- common/src/main/java/cz/incad/kramerius/processes/States.java
+++ common/src/main/java/cz/incad/kramerius/processes/States.java
@@ -1,12 +1,39 @@
 package cz.incad.kramerius.processes;
 
+/**
+ * Processes states
+ * @author pavels
+ */
 public enum States {
 	
+    /**
+     * Not running state = process just has been created
+     */
 	NOT_RUNNING(0), 
+	
+	/**
+	 * Running proces
+	 */
 	RUNNING(1), 
+	
+	/**
+	 * Correct finished proces
+	 */
 	FINISHED(2), 
+	
+	/**
+	 * FAiled with some errors
+	 */
 	FAILED(3), 
+	
+	/**
+	 * Killed process
+	 */
 	KILLED(4),
+	
+	/**
+	 * Planned = process has been created and waiting to start
+	 */
 	PLANNED(5);
 
 	public static States load(int v) {
