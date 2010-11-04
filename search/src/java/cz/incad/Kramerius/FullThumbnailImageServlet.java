1b7a3d59986f2c982ed61fb3c93b29cf9e3b27d3
pavel-stastny <pavel.stastny@gmail.com>
2010-11-04 22:17:39 +0000

    full thumbnail

--- search/src/java/cz/incad/Kramerius/FullThumbnailImageServlet.java
+++ search/src/java/cz/incad/Kramerius/FullThumbnailImageServlet.java
@@ -3,6 +3,7 @@ package cz.incad.Kramerius;
 import static cz.incad.kramerius.utils.IOUtils.copyStreams;
 import static cz.incad.utils.IKeys.UUID_PARAMETER;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -12,14 +13,23 @@ import javax.xml.xpath.XPathExpressionException;
 
 import org.w3c.dom.Document;
 
+import com.google.inject.Inject;
+
 import cz.incad.Kramerius.AbstractImageServlet.OutputFormats;
+import cz.incad.kramerius.imaging.DeepZoomTileSupport;
+import cz.incad.kramerius.imaging.lp.GenerateThumbnail;
 import cz.incad.kramerius.utils.DCUtils;
 import cz.incad.kramerius.utils.conf.KConfiguration;
+import cz.incad.kramerius.utils.imgs.ImageMimeType;
+import cz.incad.kramerius.utils.imgs.KrameriusImageSupport;
 import cz.incad.kramerius.utils.imgs.KrameriusImageSupport.ScalingMethod;
 
 public class FullThumbnailImageServlet extends AbstractImageServlet {
 
     static java.util.logging.Logger LOGGER = java.util.logging.Logger.getLogger(FullThumbnailImageServlet.class.getName());
+    
+    @Inject
+    DeepZoomTileSupport tileSupport;
     
 	@Override
 	public ScalingMethod getScalingMethod() {
@@ -49,7 +59,11 @@ public class FullThumbnailImageServlet extends AbstractImageServlet {
 	            setResponseCode(uuid, req, resp);
 	            copyStreams(fedoraAccess.getFullThumbnail(uuid), resp.getOutputStream());
 	        } else {
-	            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+	            BufferedImage scaled = GenerateThumbnail.scaleToFullThumb(uuid, fedoraAccess, tileSupport);
+                resp.setContentType(ImageMimeType.JPEG.getValue());
+	            setDateHaders(uuid, resp);
+                setResponseCode(uuid, req, resp);
+	            KrameriusImageSupport.writeImageToStream(scaled, "jpeg", resp.getOutputStream());
 	        }
         } catch (XPathExpressionException e) {
             LOGGER.severe(e.getLocalizedMessage());
