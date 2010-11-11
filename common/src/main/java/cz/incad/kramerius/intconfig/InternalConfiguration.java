c5e2d093eaf79318944f9285a6efecde13264066
pokorsky <pokorsky@gmail.com>
2010-11-11 11:51:36 +0000

    indentation

--- common/src/main/java/cz/incad/kramerius/intconfig/InternalConfiguration.java
+++ common/src/main/java/cz/incad/kramerius/intconfig/InternalConfiguration.java
@@ -2,51 +2,42 @@ package cz.incad.kramerius.intconfig;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Map;
 import java.util.Properties;
 import java.util.logging.Level;
 
-
 public class InternalConfiguration {
 
-	public static final java.util.logging.Logger LOGGER = java.util.logging.Logger
-			.getLogger(InternalConfiguration.class.getName());
-	
-	private static InternalConfiguration _internal;
-	
-	private Properties properties = new Properties();
-	
-	private InternalConfiguration() throws IOException {
-		super();
-		InputStream stream =  null;
-		try {
-			stream = this.getClass().getResourceAsStream("res/internalconfiguration.properties");
-			if(stream != null) {
-				properties.load(stream);
-			}
-		} finally {
-			if (stream != null) stream.close();
-		}
-	}
+    public static final java.util.logging.Logger LOGGER = java.util.logging.Logger.getLogger(InternalConfiguration.class.getName());
+    private static InternalConfiguration _internal;
+    private Properties properties = new Properties();
 
+    private InternalConfiguration() throws IOException {
+        super();
+        InputStream stream = null;
+        try {
+            stream = this.getClass().getResourceAsStream("res/internalconfiguration.properties");
+            if (stream != null) {
+                properties.load(stream);
+            }
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+    }
 
+    public static InternalConfiguration get() {
+        if (_internal == null) {
+            try {
+                _internal = new InternalConfiguration();
+            } catch (IOException e) {
+                LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            }
+        }
+        return _internal;
+    }
 
-	public static InternalConfiguration get() {
-		if (_internal  == null) {
-			try {
-				_internal = new InternalConfiguration();
-			} catch (IOException e) {
-				LOGGER.log(Level.SEVERE, e.getMessage(), e);
-			}
-		}
-		return _internal;
-	}
-
-
-
-	public Properties getProperties() {
-		return properties;
-	}
-
-	
+    public Properties getProperties() {
+        return properties;
+    }
 }
