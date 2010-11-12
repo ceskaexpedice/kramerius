29e5233ed240b5fbf3aa3619418fc7db0140167f
pokorsky <pokorsky@gmail.com>
2010-11-12 20:15:41 +0000

    clean up

--- search/src/java/cz/incad/Kramerius/views/adminmenu/AdminMenuViewObject.java
+++ search/src/java/cz/incad/Kramerius/views/adminmenu/AdminMenuViewObject.java
@@ -4,89 +4,103 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.ResourceBundle;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.axis.NoEndPointException;
 
 import com.google.inject.Inject;
 
 import cz.incad.Kramerius.security.KrameriusRoles;
 import cz.incad.kramerius.security.IsUserInRoleDecision;
-import cz.incad.kramerius.security.Secured;
 import cz.incad.kramerius.service.ResourceBundleService;
 
 public class AdminMenuViewObject {
 
+    public static final Logger LOGGER = Logger.getLogger(AdminMenuViewObject.class.getName());
+    @Inject
+    ResourceBundleService resourceBundleService;
+    @Inject
+    HttpServletRequest request;
+    @Inject
+    Locale locale;
+    @Inject
+    IsUserInRoleDecision userInRoleDecision;
 
-	public static final java.util.logging.Logger LOGGER = java.util.logging.Logger
-			.getLogger(AdminMenuViewObject.class.getName());
-	
-	@Inject
-	ResourceBundleService resourceBundleService;
-	@Inject
-	HttpServletRequest request;
-	@Inject
-	Locale locale;
-	@Inject
-	IsUserInRoleDecision userInRoleDecision;
+    public String processes() throws IOException {
+        return renderMenuItem(
+                "javascript:processes(); javascript:hideAdminMenu();",
+                "administrator.menu.dialogs.lrprocesses.title");
+    }
 
-	
-	public String processes() throws IOException {
-	    return "<div align=\"left\"> <a href=\"javascript:processes(); javascript:hideAdminMenu();\">"+this.resourceBundleService.getResourceBundle("labels", this.locale).getString("administrator.menu.dialogs.lrprocesses.title")+" </a> </div>";
-	}
-	
-	public String importMonographs() throws IOException {
-	    return "<div align=\"left\"> <a href=\"javascript:importMonographs(); javascript:hideAdminMenu();\">"+this.resourceBundleService.getResourceBundle("labels", this.locale).getString("administrator.menu.dialogs.importMonograph.title")+" </a> </div>";
-	}
+    public String importMonographs() throws IOException {
+        return renderMenuItem(
+                "javascript:importMonographs(); javascript:hideAdminMenu();",
+                "administrator.menu.dialogs.importMonograph.title");
+    }
 
-	public String importPeriodicals() throws IOException {
-	    return "<div align=\"left\"> <a href=\"javascript:importPeriodicals(); javascript:hideAdminMenu();\">"+this.resourceBundleService.getResourceBundle("labels", this.locale).getString("administrator.menu.dialogs.importPeriodical.title")+" </a> </div>";
-	}
+    public String importPeriodicals() throws IOException {
+        return renderMenuItem(
+                "javascript:importPeriodicals(); javascript:hideAdminMenu();",
+                "administrator.menu.dialogs.importPeriodical.title");
+    }
 
-	public String showIndexerAdmin() throws IOException {
-	    return "<div align=\"left\"> <a href=\"javascript:showIndexerAdmin(); javascript:hideAdminMenu();\">"+this.resourceBundleService.getResourceBundle("labels", this.locale).getString("administrator.menu.dialogs.indexDocuments.title")+" </a> </div>";
-	}
+    public String showIndexerAdmin() throws IOException {
+        return renderMenuItem(
+                "javascript:showIndexerAdmin(); javascript:hideAdminMenu();",
+                "administrator.menu.dialogs.indexDocuments.title");
+    }
 
-	public String noParamsProcess(String processName) throws IOException {
-	    return "<div align=\"left\"> <a href=\"javascript:noParamsProcess('"+processName+"'); javascript:hideAdminMenu();\">"+this.resourceBundleService.getResourceBundle("labels", this.locale).getString("administrator.menu.dialogs."+processName+".title")+" </a> </div>";
-	}
-	
-	public String[] getAdminMenuItems() {
-		try {
-			List<String> menuItems = new ArrayList<String>();
-			if (request.getRemoteUser() != null) {
-				if (userInRoleDecision.isUserInRole(KrameriusRoles.KRAMERIUS_ADMIN.getRoleName())) {
-					menuItems.add(processes());
-				}
-				if (userInRoleDecision.isUserInRole(KrameriusRoles.REPLIKATOR_MONOGRAPHS.getRoleName())) {
-					menuItems.add(importMonographs());
-				} 
-				if (userInRoleDecision.isUserInRole(KrameriusRoles.REPLIKATOR_PERIODICALS.getRoleName())) {
-					menuItems.add(importPeriodicals());
-				}
-				if (userInRoleDecision.isUserInRole(KrameriusRoles.REINDEX.getRoleName())) {
-					menuItems.add(showIndexerAdmin());
-				}
-				if (userInRoleDecision.isUserInRole(KrameriusRoles.ENUMERATOR.getRoleName())) {
-					menuItems.add(noParamsProcess(KrameriusRoles.ENUMERATOR.getRoleName()));
-				}
-				if (userInRoleDecision.isUserInRole(KrameriusRoles.REPLICATIONRIGHTS.getRoleName())) {
-					menuItems.add(noParamsProcess(KrameriusRoles.REPLICATIONRIGHTS.getRoleName()));
-				}
-				if (userInRoleDecision.isUserInRole(KrameriusRoles.CONVERT.getRoleName())) {
-					menuItems.add(noParamsProcess(KrameriusRoles.CONVERT.getRoleName()));
-				} 
-				if (userInRoleDecision.isUserInRole(KrameriusRoles.IMPORT.getRoleName())) {
-					menuItems.add(noParamsProcess(KrameriusRoles.IMPORT.getRoleName()));
-				}
-			}
-			return (String[]) menuItems.toArray(new String[menuItems.size()]);
-		} catch (IOException e) {
-			LOGGER.log(Level.SEVERE, e.getMessage(), e);
-			return new String[0];
-		}
-	}
+    public String noParamsProcess(String processName) throws IOException {
+        return renderMenuItem(
+                "javascript:noParamsProcess('" + processName + "'); javascript:hideAdminMenu();",
+                "administrator.menu.dialogs." + processName + ".title");
+    }
+
+    private String renderMenuItem(String href, String labelKey) throws IOException {
+        String label = this.resourceBundleService.getResourceBundle("labels", this.locale).getString(labelKey);
+        return String.format("<div align=\"left\"> <a href=\"%s\"> %s </a> </div>",
+                href, label);
+    }
+
+    public String[] getAdminMenuItems() {
+        try {
+            List<String> menuItems = new ArrayList<String>();
+            if (request.getRemoteUser() != null) {
+                if (isUserRole(KrameriusRoles.KRAMERIUS_ADMIN)) {
+                    menuItems.add(processes());
+                }
+                if (isUserRole(KrameriusRoles.REPLIKATOR_MONOGRAPHS)) {
+                    menuItems.add(importMonographs());
+                }
+                if (isUserRole(KrameriusRoles.REPLIKATOR_PERIODICALS)) {
+                    menuItems.add(importPeriodicals());
+                }
+                if (isUserRole(KrameriusRoles.REINDEX)) {
+                    menuItems.add(showIndexerAdmin());
+                }
+                if (isUserRole(KrameriusRoles.ENUMERATOR)) {
+                    menuItems.add(noParamsProcess(KrameriusRoles.ENUMERATOR.getRoleName()));
+                }
+                if (isUserRole(KrameriusRoles.REPLICATIONRIGHTS)) {
+                    menuItems.add(noParamsProcess(KrameriusRoles.REPLICATIONRIGHTS.getRoleName()));
+                }
+                if (isUserRole(KrameriusRoles.CONVERT)) {
+                    menuItems.add(noParamsProcess(KrameriusRoles.CONVERT.getRoleName()));
+                }
+                if (isUserRole(KrameriusRoles.IMPORT)) {
+                    menuItems.add(noParamsProcess(KrameriusRoles.IMPORT.getRoleName()));
+                }
+            }
+            return menuItems.toArray(new String[menuItems.size()]);
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            return new String[0];
+        }
+    }
+
+    private boolean isUserRole(KrameriusRoles role) {
+        return userInRoleDecision.isUserInRole(role.getRoleName());
+    }
 }
--- search/src/java/cz/incad/Kramerius/views/item/menu/ItemMenuViewObject.java
+++ search/src/java/cz/incad/Kramerius/views/item/menu/ItemMenuViewObject.java
@@ -2,33 +2,23 @@ package cz.incad.Kramerius.views.item.menu;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 import java.util.ResourceBundle;
-import java.util.StringTokenizer;
 import java.util.logging.Level;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.jsp.PageContext;
 
 import org.antlr.stringtemplate.StringTemplate;
 
-import com.google.inject.Inject;
 
 import cz.incad.Kramerius.FullImageServlet;
 import cz.incad.Kramerius.I18NServlet;
-import cz.incad.Kramerius.security.KrameriusRoles;
 import cz.incad.Kramerius.views.item.ItemViewObject;
 import cz.incad.kramerius.FedoraAccess;
 import cz.incad.kramerius.KrameriusModels;
-import cz.incad.kramerius.MostDesirable;
 import cz.incad.kramerius.security.IsUserInRoleDecision;
-import cz.incad.kramerius.service.ResourceBundleService;
 import cz.incad.kramerius.utils.conf.KConfiguration;
 import cz.incad.kramerius.utils.imgs.ImageMimeType;
 
@@ -37,154 +27,148 @@ import cz.incad.kramerius.utils.imgs.ImageMimeType;
  * @author pavels
  */
 public class ItemMenuViewObject {
-	
-	public static final java.util.logging.Logger LOGGER = java.util.logging.Logger
-			.getLogger(ItemMenuViewObject.class.getName());
-	
-	HttpServletRequest httpServletRequest;
-	ServletContext servletContext;
-	FedoraAccess fedoraAccess;
-	ResourceBundle resourceBundle;
-	KConfiguration kConfiguration;
-	ItemViewObject itemViewObject;
-	IsUserInRoleDecision userInRoleDecision;
 
-	Locale locale;
-	String uuid;
-	String model;
-	int index;
-	
-	
-	public ItemMenuViewObject(HttpServletRequest httpServletRequest,
-			ServletContext servletContext, FedoraAccess fedoraAccess, ResourceBundle resourceBundle, KConfiguration kConfiguration,
-			ItemViewObject itemViewObject,Locale locale, String uuid, String model ,int index, IsUserInRoleDecision userInRoleDecision) {
-		super();
-		this.httpServletRequest = httpServletRequest;
-		this.servletContext = servletContext;
-		this.fedoraAccess = fedoraAccess;
-		this.resourceBundle = resourceBundle;
-		this.kConfiguration = kConfiguration;
-		this.itemViewObject = itemViewObject;
-		this.locale = locale;
-		this.uuid = uuid;
-		this.index = index;
-		this.model = model;
-		this.userInRoleDecision = userInRoleDecision;
-	}
-	
-	public boolean isPageModel() {
-		KrameriusModels model = KrameriusModels.parseString(this.itemViewObject.getModels().get(this.index));
-		return (model.equals(KrameriusModels.PAGE));
-	}
+    public static final java.util.logging.Logger LOGGER = java.util.logging.Logger.getLogger(ItemMenuViewObject.class.getName());
+    HttpServletRequest httpServletRequest;
+    ServletContext servletContext;
+    FedoraAccess fedoraAccess;
+    ResourceBundle resourceBundle;
+    KConfiguration kConfiguration;
+    ItemViewObject itemViewObject;
+    IsUserInRoleDecision userInRoleDecision;
+    Locale locale;
+    String uuid;
+    String model;
+    int index;
 
-	private String viewMetadataItem() {
-		String key = "administrator.menu.showmetadata";
-		String jsmethod = "showMainContent";
-		return renderCommonItem(key,"none","", jsmethod);
-	}
+    public ItemMenuViewObject(HttpServletRequest httpServletRequest,
+            ServletContext servletContext, FedoraAccess fedoraAccess, ResourceBundle resourceBundle, KConfiguration kConfiguration,
+            ItemViewObject itemViewObject, Locale locale, String uuid, String model, int index, IsUserInRoleDecision userInRoleDecision) {
+        super();
+        this.httpServletRequest = httpServletRequest;
+        this.servletContext = servletContext;
+        this.fedoraAccess = fedoraAccess;
+        this.resourceBundle = resourceBundle;
+        this.kConfiguration = kConfiguration;
+        this.itemViewObject = itemViewObject;
+        this.locale = locale;
+        this.uuid = uuid;
+        this.index = index;
+        this.model = model;
+        this.userInRoleDecision = userInRoleDecision;
+    }
 
+    public boolean isPageModel() {
+        KrameriusModels model = KrameriusModels.parseString(this.itemViewObject.getModels().get(this.index));
+        return (model.equals(KrameriusModels.PAGE));
+    }
 
-	private String persistentURL() {
-		String key = "administrator.menu.persistenturl";
-		String jsmethod="showPersistentURL";
-		return renderCommonItem(key,"none","", jsmethod);
-	}
-	
+    private String viewMetadataItem() {
+        String key = "administrator.menu.showmetadata";
+        String jsmethod = "showMainContent";
+        return renderCommonItem(key, "none", "", jsmethod);
+    }
 
-	private String dynamicPDF() {
-		String key = "administrator.menu.generatepdf";
+    private String persistentURL() {
+        String key = "administrator.menu.persistenturl";
+        String jsmethod = "showPersistentURL";
+        return renderCommonItem(key, "none", "", jsmethod);
+    }
+
+    private String dynamicPDF() {
+        String key = "administrator.menu.generatepdf";
         if (isPageModel()) {
-            return renderCommonItem(key,"_data_x_ipchecked",uuid, "PDF.url");
-		} else {
-            return renderCommonItem(key,"_data_x_ipchecked",uuid, "PDF.generatePDF");
-		}
-	}
-	
-	
+            return renderCommonItem(key, "_data_x_ipchecked", uuid, "PDF.url");
+        } else {
+            return renderCommonItem(key, "_data_x_ipchecked", uuid, "PDF.generatePDF");
+        }
+    }
+
     private String downloadOriginal() throws IOException {
         String key = "administrator.menu.downloadOriginal";
-        if ((isPageModel()) || (bornDigital())){
-            return renderCommonItem(key,"_data_x_ipchecked",uuid, "downloadOriginal");
-        } else return null;
+        if ((isPageModel()) || (bornDigital())) {
+            return renderCommonItem(key, "_data_x_ipchecked", uuid, "downloadOriginal");
+        } else {
+            return null;
+        }
     }
-	
-	private String exportCD_PDF() {
-        StringTemplate template = new StringTemplate("<div align=\"left\"><a title='$tooltip$' " +
-        "href=\"javascript:generateStatic($level$,'static_export_CD','$imgServlet$','$i18nServlet$','$iso3Country$','$iso3Language$');\">$title$</a> <div style=\"display:none\">$role$</div></div>");
-	    String imgServlet = FullImageServlet.fullImageServlet(this.httpServletRequest);
-		String i18nServlet = I18NServlet.i18nServlet(this.httpServletRequest);
-		template.setAttribute("level", (index+1));
-		template.setAttribute("tooltip", this.resourceBundle.getString("administrator.menu.exportcd"));
+
+    private String exportCD_PDF() {
+        StringTemplate template = new StringTemplate("<div align=\"left\"><a title='$tooltip$' "
+                + "href=\"javascript:generateStatic($level$,'static_export_CD','$imgServlet$','$i18nServlet$','$iso3Country$','$iso3Language$');\">$title$</a> <div style=\"display:none\">$role$</div></div>");
+        String imgServlet = FullImageServlet.fullImageServlet(this.httpServletRequest);
+        String i18nServlet = I18NServlet.i18nServlet(this.httpServletRequest);
+        template.setAttribute("level", (index + 1));
+        template.setAttribute("tooltip", this.resourceBundle.getString("administrator.menu.exportcd"));
         template.setAttribute("title", this.resourceBundle.getString("administrator.menu.exportcd"));
         template.setAttribute("imgServlet", imgServlet);
         template.setAttribute("i18nServlet", i18nServlet);
         template.setAttribute("iso3Country", this.locale.getISO3Country());
         template.setAttribute("iso3Language", this.locale.getISO3Language());
         return template.toString();
-	}
+    }
 
-	private String exportDVD_PDF() {
-        StringTemplate template = new StringTemplate("<div align=\"left\"><a title='$tooltip$' " +
-        "href=\"javascript:generateStatic($level$,'static_export_DVD','$imgServlet$','$i18nServlet$','$iso3Country$','$iso3Language$');\">$title$</a> <div style=\"display:none\">$role$</div></div>");
-	    String imgServlet = FullImageServlet.fullImageServlet(this.httpServletRequest);
-		String i18nServlet = I18NServlet.i18nServlet(this.httpServletRequest);
-		template.setAttribute("level", (index+1));
-		template.setAttribute("tooltip", this.resourceBundle.getString("administrator.menu.exportdvd"));
+    private String exportDVD_PDF() {
+        StringTemplate template = new StringTemplate("<div align=\"left\"><a title='$tooltip$' "
+                + "href=\"javascript:generateStatic($level$,'static_export_DVD','$imgServlet$','$i18nServlet$','$iso3Country$','$iso3Language$');\">$title$</a> <div style=\"display:none\">$role$</div></div>");
+        String imgServlet = FullImageServlet.fullImageServlet(this.httpServletRequest);
+        String i18nServlet = I18NServlet.i18nServlet(this.httpServletRequest);
+        template.setAttribute("level", (index + 1));
+        template.setAttribute("tooltip", this.resourceBundle.getString("administrator.menu.exportdvd"));
         template.setAttribute("title", this.resourceBundle.getString("administrator.menu.exportdvd"));
         template.setAttribute("imgServlet", imgServlet);
         template.setAttribute("i18nServlet", i18nServlet);
         template.setAttribute("iso3Country", this.locale.getISO3Country());
         template.setAttribute("iso3Language", this.locale.getISO3Language());
         return template.toString();
-	}
+    }
 
-	
-	private String exportMETS() {
-		String key = "administrator.menu.exportMETS";
-		String jsmethod = "showMets";
-		return renderCommonItem(key,"none","", jsmethod);
-	}
-	
-	private String reindex() {
-		String key = "administrator.menu.reindex";
-		String jsmethod = "reindex";
-		return renderCommonItem(key,"_data_x_role", "reindex", jsmethod);
-	}
+    private String exportMETS() {
+        String key = "administrator.menu.exportMETS";
+        String jsmethod = "showMets";
+        return renderCommonItem(key, "none", "", jsmethod);
+    }
 
-	private String deleteFromIndex() {
-	    String jsmethod = "deletefromindex";
-		String key = "administrator.menu.deletefromindex";
-		return renderCommonItem(key,"_data_x_role","reindex", jsmethod);
-	}
+    private String reindex() {
+        String key = "administrator.menu.reindex";
+        String jsmethod = "reindex";
+        return renderCommonItem(key, "_data_x_role", "reindex", jsmethod);
+    }
 
-	private String generateDeepZoomTiles() {
-	    String jsmethod = "generateDeepZoomTiles";
-		String key = "administrator.menu.generateDeepZoomTiles";
-		return renderCommonItem(key,"_data_x_role","reindex", jsmethod);
-	}
+    private String deleteFromIndex() {
+        String jsmethod = "deletefromindex";
+        String key = "administrator.menu.deletefromindex";
+        return renderCommonItem(key, "_data_x_role", "reindex", jsmethod);
+    }
 
-	private String deleteFromFedora() {
-	    String key = "administrator.menu.deleteuuid";
-	    String jsmethod = "deleteUuid";
-	    return renderCommonItem(key,"_data_x_role","delete", jsmethod);
-	}
+    private String generateDeepZoomTiles() {
+        String jsmethod = "generateDeepZoomTiles";
+        String key = "administrator.menu.generateDeepZoomTiles";
+        return renderCommonItem(key, "_data_x_role", "reindex", jsmethod);
+    }
 
-	private String changeVisibility() {
-	    String key = "administrator.menu.setpublic";
-	    String jsmethod = "changeFlag";
-        return renderCommonItem(key,"_data_x_role","setprivate", jsmethod);
-	}
+    private String deleteFromFedora() {
+        String key = "administrator.menu.deleteuuid";
+        String jsmethod = "deleteUuid";
+        return renderCommonItem(key, "_data_x_role", "delete", jsmethod);
+    }
 
-	private String exportTOFOXML() {
+    private String changeVisibility() {
+        String key = "administrator.menu.setpublic";
+        String jsmethod = "changeFlag";
+        return renderCommonItem(key, "_data_x_role", "setprivate", jsmethod);
+    }
+
+    private String exportTOFOXML() {
         String key = "administrator.menu.exportFOXML";
         String jsmethod = "exportTOFOXML";
-        return renderCommonItem(key,"_data_x_role", "export", jsmethod);
-	}
+        return renderCommonItem(key, "_data_x_role", "export", jsmethod);
+    }
 
     private String renderCommonItem(String key, String dataType, String value, String jsmethod) {
         StringTemplate template = getCommonTemplate();
         jsmethod(template, jsmethod);
-	    titleAndTooltip(template, key);
+        titleAndTooltip(template, key);
         levelAndModel(template);
         template.setAttribute("datatype", dataType);
         template.setAttribute("value", value);
@@ -192,9 +176,9 @@ public class ItemMenuViewObject {
     }
 
     private StringTemplate getCommonTemplate() {
-        StringTemplate template = new StringTemplate("<div align=\"left\"><a title='$tooltip$' " +
-	    		"href=\"javascript:$jsmethod$($level$,'$model$');\">$title$</a> " +
-	    		"<div class=\"$datatype$\" style=\"display:none\">$value$</div></div>");
+        StringTemplate template = new StringTemplate("<div align=\"left\"><a title='$tooltip$' "
+                + "href=\"javascript:$jsmethod$($level$,'$model$');\">$title$</a> "
+                + "<div class=\"$datatype$\" style=\"display:none\">$value$</div></div>");
         return template;
     }
 
@@ -209,41 +193,38 @@ public class ItemMenuViewObject {
 
     private void levelAndModel(StringTemplate template) {
         template.setAttribute("model", this.itemViewObject.getModels().get(this.index));
-        template.setAttribute("level", (index+1));
+        template.setAttribute("level", (index + 1));
     }
-	
-	/**
-	 * Create menu items
-	 * @return
-	 */
-	public String[] getContextMenuItems() {
-	    List<String> items = new ArrayList<String>();
-	    items.addAll(getCommonMenuItems());
-		items.addAll(getAdminMenuItems());
-		return (String[]) items.toArray(new String[items.size()]);
-	}
+
+    /**
+     * Create menu items
+     * @return
+     */
+    public String[] getContextMenuItems() {
+        List<String> items = new ArrayList<String>();
+        items.addAll(getCommonMenuItems());
+        items.addAll(getAdminMenuItems());
+        return (String[]) items.toArray(new String[items.size()]);
+    }
 
     private List<String> getCommonMenuItems() {
         List<String> items = new ArrayList<String>();
-		{
-			items.add(viewMetadataItem());
-			items.add(persistentURL());
-			try {
-		        if (!bornDigital()) {
-                    items.add(dynamicPDF());
-		        }
-                if ((isPageModel()) || (bornDigital())) {
-                    items.add(downloadOriginal());
-                }
-			} catch (IOException e) {
-				LOGGER.log(Level.SEVERE, e.getMessage(), e);
-			}
-			items.add(exportMETS());
-		};
+        items.add(viewMetadataItem());
+        items.add(persistentURL());
+        try {
+            if (!bornDigital()) {
+                items.add(dynamicPDF());
+            }
+            if ((isPageModel()) || (bornDigital())) {
+                items.add(downloadOriginal());
+            }
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+        }
+        items.add(exportMETS());
         return items;
     }
 
-    
     /**
      * Tests whether document is born digital document (only PDF at this moment) 
      * @return
@@ -254,9 +235,9 @@ public class ItemMenuViewObject {
         boolean bornDigital = false;
         boolean imageFullContains = fedoraAccess.isImageFULLAvailable(uuid);
         if (imageFullContains) {
-            String smimeType = fedoraAccess.getMimeTypeForStream("uuid:"+uuid, "IMG_FULL");
+            String smimeType = fedoraAccess.getMimeTypeForStream("uuid:" + uuid, "IMG_FULL");
             ImageMimeType mimeType = ImageMimeType.loadFromMimeType(smimeType);
-            if ((mimeType != null) && (mimeType == ImageMimeType.PDF)){
+            if ((mimeType != null) && (mimeType == ImageMimeType.PDF)) {
                 bornDigital = true;
             }
         }
@@ -275,43 +256,39 @@ public class ItemMenuViewObject {
             items.add(exportDVD_PDF());
             items.add(generateDeepZoomTiles());
             items.add(deleteGenerateDeepZoomTiles());
-		}
+        }
         return items;
     }
-	
-
 
     public String getUuid() {
-		return this.uuid;
-	}
-	
-	public String getModel() {
-		return this.model;
-	}
-	
-	public int getIndex() {
-		return this.index;
-	}
-	
+        return this.uuid;
+    }
 
-	
+    public String getModel() {
+        return this.model;
+    }
+
+    public int getIndex() {
+        return this.index;
+    }
+
     private String deleteGenerateDeepZoomTiles() {
         String jsmethod = "deleteGeneratedDeepZoomTiles";
         String key = "administrator.menu.deleteGeneratedDeepZoomTiles";
-        return renderCommonItem(key,"_data_x_role","reindex", jsmethod);
+        return renderCommonItem(key, "_data_x_role", "reindex", jsmethod);
     }
-	
-	// TODO: Presunout jinam
-	public String getBiblioModsURL() {
+
+    // TODO: Presunout jinam
+    public String getBiblioModsURL() {
         //<c:out value="${kconfig.fedoraHost}" />/get/uuid:<c:out value="${uuid}" />/BIBLIO_MODS
-		return this.kConfiguration.getFedoraHost()+"/get/uuid:"+this.uuid+"/BIBLIO_MODS";
-	}
-	
-	public String getUpdateSelection() {
-	    StringTemplate template = new StringTemplate("setSelection($level$,'$model$','$uuid$');");
-	    template.setAttribute("level", (index+1));
-	    template.setAttribute("model", model);
-	    template.setAttribute("uuid", uuid);
-	    return template.toString();
-	}
+        return this.kConfiguration.getFedoraHost() + "/get/uuid:" + this.uuid + "/BIBLIO_MODS";
+    }
+
+    public String getUpdateSelection() {
+        StringTemplate template = new StringTemplate("setSelection($level$,'$model$','$uuid$');");
+        template.setAttribute("level", (index + 1));
+        template.setAttribute("model", model);
+        template.setAttribute("uuid", uuid);
+        return template.toString();
+    }
 }
