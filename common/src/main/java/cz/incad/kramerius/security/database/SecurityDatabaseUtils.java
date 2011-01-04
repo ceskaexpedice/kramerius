b7fe6b02c29500f9f91db0345fc6659713e8346c
pavel-stastny <pavel.stastny@gmail.com>
2011-01-04 09:08:30 +0000

    database utils

--- common/src/main/java/cz/incad/kramerius/security/database/SecurityDatabaseUtils.java
+++ common/src/main/java/cz/incad/kramerius/security/database/SecurityDatabaseUtils.java
@@ -28,13 +28,10 @@ import org.antlr.stringtemplate.language.DefaultTemplateLexer;
 public class SecurityDatabaseUtils {
 
     static java.util.logging.Logger LOGGER = java.util.logging.Logger.getLogger(SecurityDatabaseUtils.class.getName());
-    
 
     public static StringTemplateGroup stGroup() {
         InputStream is = SecurityDatabaseUtils.class.getResourceAsStream("database.stg");
         StringTemplateGroup grp = new StringTemplateGroup(new InputStreamReader(is), DefaultTemplateLexer.class);
         return grp;
     }
-
-    
 }
