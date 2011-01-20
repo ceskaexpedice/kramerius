3b568293a55d9693f38c626949fbe902368b2bc9
pavel-stastny <pavel.stastny@gmail.com>
2011-01-20 15:53:25 +0000

    changes in users editor

--- rightseditor/src/cz/incad/kramerius/rights/server/arragements/UserArrangement.java
+++ rightseditor/src/cz/incad/kramerius/rights/server/arragements/UserArrangement.java
@@ -13,23 +13,23 @@ import org.aplikator.server.descriptor.VerticalPanel;
 import cz.incad.kramerius.rights.server.Structure;
 import cz.incad.kramerius.rights.server.Structure.UserEntity;
 
-public class UserArrangement extends Arrangement{
+public class UserArrangement extends Arrangement {
 
 	Structure struct;
 	Structure.UserEntity userEntity;
 	GroupArrangement groupArrangement;
-	
-	public UserArrangement(Structure struct, UserEntity entity, GroupArrangement groupArrangement) {
+
+	public UserArrangement(Structure struct, UserEntity entity,
+			GroupArrangement groupArrangement) {
 		super(entity);
 		this.struct = struct;
 		this.userEntity = entity;
 		this.groupArrangement = groupArrangement;
-		
+
 		setReadableName(struct.user.getName());
 
-		addProperty(struct.user.NAME).addProperty(
-				struct.user.SURNAME).addProperty(
-				struct.user.LOGINNAME);
+		addProperty(struct.user.NAME).addProperty(struct.user.SURNAME)
+				.addProperty(struct.user.LOGINNAME);
 
 		queryGenerator = new QueryGenerator.Empty();
 
@@ -37,34 +37,32 @@ public class UserArrangement extends Arrangement{
 
 	}
 
-	
 	private Form createUserForm() {
 		Form form = new Form();
 		form.setLayout(new VerticalPanel()
-					.addChild(new VerticalPanel()
-									.addChild(new TextField(struct.user.NAME))
-									.addChild(new TextField(struct.user.SURNAME))
-					)
-				
-					.addChild(new VerticalPanel()
-						.addChild(new TextField(struct.user.LOGINNAME))
-						.addChild(new TextField(struct.user.PASSWORD))
-					)
+				.addChild(
+						new VerticalPanel().addChild(
+								new TextField(struct.user.NAME)).addChild(
+								new TextField(struct.user.SURNAME)))
 
-					.addChild(new RepeatedForm(
-							struct.user.ASSOC_FOR_USRGRP, groupArrangement))
+				.addChild(
+						new VerticalPanel().addChild(
+								new TextField(struct.user.LOGINNAME)).addChild(
+								new TextField(struct.user.PASSWORD)))
 
-					.addChild(
-							new RefButton(struct.user.PERSONAL_ADMIN,
-									this.groupArrangement,
-									new HorizontalPanel().addChild(new TextField(
-											struct.user.PERSONAL_ADMIN
-													.relate(struct.group.GNAME)))))
-		
+				.addChild(
+						new RepeatedForm(struct.user.ASSOC_FOR_USRGRP,
+								groupArrangement))
 
-	);
+				.addChild(
+						new RefButton(struct.user.PERSONAL_ADMIN,
+								this.groupArrangement,
+								new HorizontalPanel().addChild(new TextField(
+										struct.user.PERSONAL_ADMIN
+												.relate(struct.group.GNAME)))))
+
+		);
 		return form;
 	}
 
-	
 }
