a0777254711d065cef572472d88947896969b5df
pavel-stastny <pavel.stastny@gmail.com>
2011-02-02 14:37:58 +0000

    conditions in query

--- rightseditor/src/cz/incad/kramerius/rights/server/arragements/GroupArrangement.java
+++ rightseditor/src/cz/incad/kramerius/rights/server/arragements/GroupArrangement.java
@@ -1,9 +1,15 @@
 package cz.incad.kramerius.rights.server.arragements;
 
 
+import java.util.List;
+
+import org.apache.empire.db.expr.compare.DBCompareExpr;
+import org.aplikator.client.descriptor.QueryParameter;
+import org.aplikator.server.Context;
 import org.aplikator.server.descriptor.Arrangement;
 import org.aplikator.server.descriptor.Form;
 import org.aplikator.server.descriptor.HorizontalPanel;
+import org.aplikator.server.descriptor.QueryGenerator;
 import org.aplikator.server.descriptor.RefButton;
 import org.aplikator.server.descriptor.RepeatedForm;
 import org.aplikator.server.descriptor.TextArea;
@@ -13,12 +19,16 @@ import org.aplikator.server.descriptor.VerticalPanel;
 import cz.incad.kramerius.rights.server.Structure;
 import cz.incad.kramerius.rights.server.Structure.GroupEntity;
 import cz.incad.kramerius.rights.server.arragements.triggers.GroupTriggers;
+import cz.incad.kramerius.rights.server.utils.GetAdminGroupIds;
+import cz.incad.kramerius.rights.server.utils.GetCurrentLoggedUser;
+import cz.incad.kramerius.security.User;
 
 public class GroupArrangement extends Arrangement {
 
 	Structure struct;
 	Structure.GroupEntity groupEntity;
 	RefenrenceToPersonalAdminArrangement reference;
+	Form form;
 	
 	public GroupArrangement(Structure structure, GroupEntity entity, RefenrenceToPersonalAdminArrangement reference) {
 		super(entity);
@@ -28,12 +38,40 @@ public class GroupArrangement extends Arrangement {
 		setReadableName(struct.group.getName());
 		addProperty(struct.group.GNAME);
 		setSortProperty(struct.group.GNAME);
-		setForm(createGroupForm());
+		//setForm(createGroupForm());
+		setQueryGenerator(new GroupQueryGenerator());
 		this.trigger = new GroupTriggers(this.struct);
+	} 
+	
+	
+	
+	@Override
+	public synchronized Form getForm(Context context) {
+		Form form = null;
+		User user = GetCurrentLoggedUser.getCurrentLoggedUser(context.getHttpServletRequest());
+		if ((user != null) && (user.hasSuperAdministratorRole())) {
+			form = createAdminGroupForm();
+		} else {
+			form = createSubadminGroupForm();
+		}
+		return form;
 	}
-	
-	
-	private Form createGroupForm() {
+
+
+	private Form createSubadminGroupForm() {
+		Form form = new Form();
+		form.setLayout(new VerticalPanel()
+				.addChild(new TextField(struct.group.GNAME))
+				.addChild(new TextArea(struct.group.DESCRIPTION).setWidth("100%"))
+				.addChild(new RepeatedForm(struct.group.USER_ASSOCIATIONS, new GroupUsersArrangement()))
+
+		);
+		form.addProperty(struct.group.PERSONAL_ADMIN);
+		return form;
+		
+	}
+
+	private Form createAdminGroupForm() {
 		Form form = new Form();
 		form.setLayout(new VerticalPanel()
 					.addChild(new TextField(struct.group.GNAME))
@@ -51,7 +89,7 @@ public class GroupArrangement extends Arrangement {
 	}
 	
 
-	
+	/** uzivatele skupiny */
 	public class GroupUsersArrangement extends Arrangement{
         
         public GroupUsersArrangement(){
@@ -61,6 +99,7 @@ public class GroupArrangement extends Arrangement {
             //addProperty(structure.groupUserAssoction.GROUP);
             addProperty(struct.groupUserAssoction.USERS.relate(struct.user.LOGINNAME));
             setForm(createForm());
+            //setQueryGenerator(new FormUsersGenerator());
         }
         
         Form createForm() {
@@ -78,6 +117,9 @@ public class GroupArrangement extends Arrangement {
             return form;
         }
         
+
+        
+        
     }
 	
 	public class RefUserArrangement extends Arrangement{
@@ -87,6 +129,7 @@ public class GroupArrangement extends Arrangement {
             addProperty(struct.user.LOGINNAME);
             setSortProperty(struct.user.LOGINNAME);
             setForm(createUserForm());
+            setQueryGenerator(new RefUsersGenerator());
         }
         
         
@@ -116,5 +159,45 @@ public class GroupArrangement extends Arrangement {
             return form;
         }
         
+
+        public class RefUsersGenerator implements QueryGenerator {
+
+			@Override
+			public QueryParameter[] getQueryParameters(Context ctx) {
+	            return new QueryParameter[]{};
+			}
+
+			@Override
+			public DBCompareExpr createWhere(QueryParameter[] queryParameters,
+					Context ctx) {
+	        	User user = GetCurrentLoggedUser.getCurrentLoggedUser(ctx.getHttpServletRequest());
+	        	if (!user.hasSuperAdministratorRole()) {
+		        	List<Integer> admId = GetAdminGroupIds.getAdminGroupId(ctx);
+		        	return struct.user.PERSONAL_ADMIN.column.is(admId.get(0));
+	        	} else {
+	        		return null;
+	        	}
+	        	
+			}
+   	 	}
+
+        
     }
+	
+	
+	 public class GroupQueryGenerator implements QueryGenerator {
+	        
+	        public QueryParameter[] getQueryParameters(Context ctx){
+	            return new QueryParameter[]{};
+	        }
+	        
+	        public DBCompareExpr createWhere(QueryParameter[] queryParameters, Context ctx) {
+	        	User user = GetCurrentLoggedUser.getCurrentLoggedUser(ctx.getHttpServletRequest());
+	        	if (!user.hasSuperAdministratorRole()) {
+		        	List<Integer> admId = GetAdminGroupIds.getAdminGroupId(ctx);
+		        	return struct.group.PERSONAL_ADMIN.column.is(admId.get(0));
+	        	} else return null;
+	        }
+	    }
+
 }
