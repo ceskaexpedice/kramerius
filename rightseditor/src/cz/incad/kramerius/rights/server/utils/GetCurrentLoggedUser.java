83af8ab293926e78ac416bf34d8f417c7c1a538c
pavel-stastny <pavel.stastny@gmail.com>
2011-02-02 14:38:27 +0000

    new utils classes

---
+++ rightseditor/src/cz/incad/kramerius/rights/server/utils/GetCurrentLoggedUser.java
@@ -0,0 +1,49 @@
+package cz.incad.kramerius.rights.server.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+import cz.incad.kramerius.security.Group;
+import cz.incad.kramerius.security.User;
+import cz.incad.kramerius.security.impl.GroupImpl;
+import cz.incad.kramerius.security.impl.UserImpl;
+
+public class GetCurrentLoggedUser {
+
+	public static User getCurrentLoggedUser(HttpServletRequest request) {
+			return GetCurrentLoggedUser.getJosefVomacka();
+	//		if (Boolean.getBoolean(DEBUG_KEY)) {
+	//			// pouze pro debug
+	//			return getKarelPoslusny();
+	//		} else {
+	//			Principal principal = request.getUserPrincipal();
+	//	        if (principal != null) {
+	//	            K4UserPrincipal k4principal = (K4UserPrincipal) principal;
+	//	            User user = k4principal.getUser();
+	//	            return user;
+	//	        } else return null;		
+	//		}
+		}
+
+	public static User getJosefVomacka() {
+		UserImpl userImpl = new UserImpl(1, "josef", "vomacka", "josef.vomacka@mzz.cz", 0);	
+		Group knav_subadmins = new cz.incad.kramerius.security.impl.GroupImpl(4,"knav_subadmins",3);
+		userImpl.setGroups(new Group[] {knav_subadmins});
+		return userImpl;
+	}
+
+	public static User getPavelStastny() {
+		UserImpl userImpl = new UserImpl(2, "Pavel", "Stastny", "pavels@incad.cz", 0);	
+		Group k4_admins = new cz.incad.kramerius.security.impl.GroupImpl(3,"k4_admins",0);
+		Group knav_users = new cz.incad.kramerius.security.impl.GroupImpl(2,"knav_users",4);
+		userImpl.setGroups(new Group[] {k4_admins, knav_users});
+		return userImpl;
+	}
+
+	public static User getKarelPoslusny() {
+		UserImpl userImpl = new UserImpl(3, "Karel", "Poslusny", "karels@poslusny.cz", 0);	
+		Group knav_users = new cz.incad.kramerius.security.impl.GroupImpl(2,"knav_users",4);
+		userImpl.setGroups(new Group[] { knav_users});
+		return userImpl;
+	}
+
+}
