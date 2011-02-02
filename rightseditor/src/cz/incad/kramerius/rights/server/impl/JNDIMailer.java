4d553061ba9c8397e7a2bbc063f1dbecb14e496a
pavel-stastny <pavel.stastny@gmail.com>
2011-02-02 10:23:15 +0000

    mailer

---
+++ rightseditor/src/cz/incad/kramerius/rights/server/impl/JNDIMailer.java
@@ -0,0 +1,15 @@
+package cz.incad.kramerius.rights.server.impl;
+
+import javax.mail.Session;
+
+import cz.incad.kramerius.rights.server.Mailer;
+
+public class JNDIMailer extends Mailer {
+
+	@Override
+	public Session getSession(String name, String pass) {
+		// session from jndi
+		return null;
+	}
+
+}
