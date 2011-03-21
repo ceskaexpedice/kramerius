5a64879a7b5c232cad01e65062c25de3d3269335
pavel-stastny <pavel.stastny@gmail.com>
2011-03-21 15:40:16 +0000

    delete unused classes

--- common/src/test/java/cz/incad/kramerius/service/ResourceBundleServiceModule.java
+++ common/src/test/java/cz/incad/kramerius/service/ResourceBundleServiceModule.java
@@ -1,26 +1,9 @@
 package cz.incad.kramerius.service;
 
-import java.sql.Connection;
-import java.util.Locale;
-
-
 import com.google.inject.AbstractModule;
-import com.google.inject.Scopes;
 import com.google.inject.name.Names;
 
-import cz.incad.kramerius.FedoraAccess;
-import cz.incad.kramerius.impl.FedoraAccessImpl;
-import cz.incad.kramerius.pdf.GeneratePDFService;
-import cz.incad.kramerius.pdf.impl.GeneratePDFServiceImpl;
-import cz.incad.kramerius.processes.database.JNDIConnectionProvider;
-import cz.incad.kramerius.security.SecuredFedoraAccessImpl;
-import cz.incad.kramerius.security.IPaddressChecker;
-import cz.incad.kramerius.service.impl.DeleteServiceImpl;
-import cz.incad.kramerius.service.impl.ExportServiceImpl;
-import cz.incad.kramerius.service.impl.PolicyServiceImpl;
 import cz.incad.kramerius.service.impl.ResourceBundleServiceImpl;
-import cz.incad.kramerius.service.impl.TextsServiceImpl;
-import cz.incad.kramerius.utils.conf.KConfiguration;
 
 public class ResourceBundleServiceModule extends AbstractModule {
 
