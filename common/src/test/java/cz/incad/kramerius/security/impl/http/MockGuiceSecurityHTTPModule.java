aa1c9c0f547652d6f217d26c1d5757a75625c28a
pavel-stastny <pavel.stastny@gmail.com>
2011-05-16 12:18:36 +0000

    Issue 49-tests

---
+++ common/src/test/java/cz/incad/kramerius/security/impl/http/MockGuiceSecurityHTTPModule.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2010 Pavel Stastny
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.incad.kramerius.security.impl.http;
+
+import static org.easymock.EasyMock.createMockBuilder;
+import static org.easymock.EasyMock.replay;
+
+import org.easymock.EasyMock;
+
+import com.google.inject.AbstractModule;
+
+import cz.incad.kramerius.FedoraAccess;
+import cz.incad.kramerius.fedora.impl.DataPrepare;
+import cz.incad.kramerius.impl.FedoraAccessImpl;
+import cz.incad.kramerius.security.IsActionAllowed;
+import cz.incad.kramerius.security.SecuredActions;
+import cz.incad.kramerius.security.User;
+import cz.incad.kramerius.utils.conf.KConfiguration;
+
+public class MockGuiceSecurityHTTPModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        IsActionAllowed isAllowed = EasyMock.createMock(IsActionAllowed.class);
+        EasyMock.expect(isAllowed.isActionAllowed(SecuredActions.READ.getFormalName(), "1", new String[]{"1"})).andReturn(true);
+        replay(isAllowed);
+
+        bind(IsActionAllowed.class).toInstance(isAllowed);
+        bind(User.class).toProvider(MockUserProvider.class);
+    }
+}
