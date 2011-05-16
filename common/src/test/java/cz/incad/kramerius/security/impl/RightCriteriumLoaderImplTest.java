8073ee013ed4c0c7476440816cc052b9145c0def
pavel-stastny <pavel.stastny@gmail.com>
2011-05-16 12:18:19 +0000

    Issue 49-tests

---
+++ common/src/test/java/cz/incad/kramerius/security/impl/RightCriteriumLoaderImplTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2010 Pavel Stastny
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.incad.kramerius.security.impl;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import cz.incad.kramerius.security.RightCriterium;
+
+public class RightCriteriumLoaderImplTest  {
+
+    @Before
+    public void setup() {
+        
+    }
+
+    @Test
+    public void testRightCriterium() {
+        ClassRightCriteriumLoaderImpl rr = new ClassRightCriteriumLoaderImpl();
+        List<RightCriterium> criteriums = rr.getCriteriums();
+        Assert.assertFalse(criteriums.isEmpty());
+    }
+}
