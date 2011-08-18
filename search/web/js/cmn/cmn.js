5d5476bba81c31a32e6c6fbbc4e705ffa2923cf7
pavel-stastny <pavel.stastny@gmail.com>
2011-08-18 08:58:40 +0000

    removed dictionary object

--- search/web/js/cmn/cmn.js
+++ search/web/js/cmn/cmn.js
@@ -2,30 +2,6 @@
  * @fileoverview <h3>Common functions using everywhere</h3>
  */
 
-/**
- * Dictionary class
- * @class
- */
-function Dictionary(startValues) {
-	this.values = startValues || {};
-}
-
-Dictionary.prototype.store = function(name, value) {
-	this.values[name] = value;
-};
-
-Dictionary.prototype.lookup = function(name) {
-	return this.values[name];
-};
-
-Dictionary.prototype.contains = function(name) {
-	return Object.prototype.hasOwnProperty.call(this.values, name) &&
-	Object.prototype.propertyIsEnumerable.call(this.values, name);
-};
-
-Dictionary.prototype.each = function(action) {
-	$.each(this.values, action);
-};
 
 
 /** forEach in array */
