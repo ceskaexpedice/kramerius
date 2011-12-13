cbc110604b4a10b22db2d7a71417533d72a3980d
pavel-stastny <pavel.stastny@gmail.com>
2011-12-13 14:16:41 +0000

    removed dependencies

--- search/src/java/cz/incad/Kramerius/backend/guice/GuiceConfigBean.java
+++ search/src/java/cz/incad/Kramerius/backend/guice/GuiceConfigBean.java
@@ -1,17 +1,9 @@
 package cz.incad.Kramerius.backend.guice;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletContextEvent;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.google.inject.grapher.GrapherModule;
-import com.google.inject.grapher.InjectorGrapher;
-import com.google.inject.grapher.graphviz.GraphvizModule;
-import com.google.inject.grapher.graphviz.GraphvizRenderer;
 import com.google.inject.servlet.GuiceServletContextListener;
 import com.google.inject.servlet.ServletModule;
 
@@ -62,20 +54,21 @@ public class GuiceConfigBean extends GuiceServletContextListener {
         return injector;
     }
 
-    public static class Grapher {
-
-        public void graph(String filename, Injector demoInjector) throws IOException {
-        
-            PrintWriter out = new PrintWriter(new File(filename), "UTF-8");
-            Injector injector = Guice.createInjector(new GrapherModule(), new GraphvizModule());
-            GraphvizRenderer renderer = injector.getInstance(GraphvizRenderer.class);
-
-            renderer.setOut(out).setRankdir("TB");
-
-            injector.getInstance(InjectorGrapher.class)
-              .of(demoInjector)
-              .graph();
-            }
-      }
+    
+//    public static class Grapher {
+//
+//        public void graph(String filename, Injector demoInjector) throws IOException {
+//        
+//            PrintWriter out = new PrintWriter(new File(filename), "UTF-8");
+//            Injector injector = Guice.createInjector(new GrapherModule(), new GraphvizModule());
+//            GraphvizRenderer renderer = injector.getInstance(GraphvizRenderer.class);
+//
+//            renderer.setOut(out).setRankdir("TB");
+//
+//            injector.getInstance(InjectorGrapher.class)
+//              .of(demoInjector)
+//              .graph();
+//            }
+//      }
 
 }
