867a149bd17c3ed598d3e613838fd186b3543447
vlahoda <vlahoda@gmail.com>
2012-01-18 21:16:02 +0000

    updated Aplikator to version 0.2

--- security-core/src/main/java/cz/incad/kramerius/security/jaas/K4LoginModule.java
+++ security-core/src/main/java/cz/incad/kramerius/security/jaas/K4LoginModule.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2010 Pavel Stastny
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -37,9 +37,7 @@ import javax.security.auth.callback.UnsupportedCallbackException;
 import javax.security.auth.login.LoginException;
 import javax.security.auth.spi.LoginModule;
 
-import cz.incad.kramerius.security.Role;
 import cz.incad.kramerius.security.User;
-import cz.incad.kramerius.security.impl.UserImpl;
 import cz.incad.kramerius.security.utils.PasswordDigest;
 import cz.incad.kramerius.security.utils.SecurityDBUtils;
 import cz.incad.kramerius.utils.database.JDBCQueryTemplate;
--- security-core/src/main/java/cz/incad/kramerius/security/jaas/K4User.java
+++ security-core/src/main/java/cz/incad/kramerius/security/jaas/K4User.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2010 Pavel Stastny
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -20,6 +20,8 @@ import java.io.Serializable;
 import java.security.Principal;
 
 public class K4User implements Principal, Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private String userIdent;
 
@@ -38,5 +40,5 @@ public class K4User implements Principal, Serializable {
         return this.userIdent;
     }
 
-    
+
 }
