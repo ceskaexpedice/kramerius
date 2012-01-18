25c703aa847bbd2644073b0e45f16d47dac90d3e
vlahoda <vlahoda@gmail.com>
2012-01-18 21:25:57 +0000

    updated Aplikator to version 0.2

--- rightseditor/src/cz/incad/kramerius/rights/server/utils/I18NUtils.java
+++ rightseditor/src/cz/incad/kramerius/rights/server/utils/I18NUtils.java
@@ -3,12 +3,12 @@ package cz.incad.kramerius.rights.server.utils;
 import java.util.Locale;
 
 import org.aplikator.server.Context;
-import org.aplikator.server.descriptor.Application;
+import org.aplikator.server.util.Configurator;
 
 public class I18NUtils {
 
     public static String getLocalizedString(String key, Context ctx) {
-        return Application.get().getLocalizedString(key, ctx != null ? ctx.getUserLocale() : Locale.getDefault());
+        return Configurator.get().getLocalizedString(key, ctx != null ? ctx.getUserLocale() : Locale.getDefault());
     }
 
 }
