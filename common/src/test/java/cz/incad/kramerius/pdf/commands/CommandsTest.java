aab8eaa0d4b4fafa2389c1714631f8c21a763a96
pavel-stastny <pavel.stastny@gmail.com>
2012-04-22 04:59:14 +0000

    commands test

--- common/src/test/java/cz/incad/kramerius/pdf/commands/CommandsTest.java
+++ common/src/test/java/cz/incad/kramerius/pdf/commands/CommandsTest.java
@@ -16,11 +16,13 @@
  */
 package cz.incad.kramerius.pdf.commands;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -31,6 +33,7 @@ import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 import cz.incad.kramerius.pdf.Break;
+import cz.incad.kramerius.utils.IOUtils;
 import cz.incad.kramerius.utils.XMLUtils;
 
 public class CommandsTest {
@@ -41,10 +44,10 @@ public class CommandsTest {
         InputStream stream = this.getClass().getResourceAsStream("commands.xml");
         Document document = XMLUtils.parseDocument(stream);
         
-        Commands cmnds = new Commands();
+        ITextCommands cmnds = new ITextCommands();
         cmnds.load(document.getDocumentElement(), cmnds);
         
-        List<Command> commands = cmnds.getCommands();
+        List<ITextCommand> commands = cmnds.getCommands();
         Assert.assertTrue(commands.size() == 4);
 
         Assert.assertEquals(commands.get(0).getClass(), Paragraph.class);
@@ -55,87 +58,109 @@ public class CommandsTest {
     
     
     @Test
-    public void testCommandsVisitor() throws ParserConfigurationException, SAXException, IOException, InstantiationException, IllegalAccessException {
+    public void testCommandsListener() throws ParserConfigurationException, SAXException, IOException, InstantiationException, IllegalAccessException {
         InputStream stream = this.getClass().getResourceAsStream("commands.xml");
         Document document = XMLUtils.parseDocument(stream);
 
-        Commands cmnds = new Commands();
+        ITextCommands cmnds = new ITextCommands();
         cmnds.load(document.getDocumentElement(), cmnds);
-        List<Command> list = cmnds.getCommands();
-        
-        
-        CommandVisitor visitor = new CommandVisitor() {
-            
-            @Override
-            public Object visit(Image image, Object obj) {
-                List<String> visited = (List<String>) obj;
-                visited.add(image.getClass().getName());
-                return visited;
-            }
-            
-            @Override
-            public Object visit(Commands cmnds, Object obj) {
-                List<String> visited = (List<String>) obj;
-                visited.add(cmnds.getClass().getName());
-                return visited;
-            }
-            
-            @Override
-            public Object visit(Paragraph text, Object obj) {
-                List<String> visited = (List<String>) obj;
-                visited.add(text.getClass().getName());
-                return visited;
-            }
-            
-            @Override
-            public Object visit(ListItem listItem, Object obj) {
-                List<String> visited = (List<String>) obj;
-                visited.add(listItem.getClass().getName());
-                return visited;
-            }
-            
-            @Override
-            public Object visit(cz.incad.kramerius.pdf.commands.List list, Object obj) {
-                List<String> visited = (List<String>) obj;
-                visited.add(list.getClass().getName());
-                return visited;
-            }
-            
-            @Override
-            public Object visit(Line line, Object obj) {
-                List<String> visited = (List<String>) obj;
-                visited.add(line.getClass().getName());
-                return visited;
-            }
-
-            @Override
-            public Object visit(Text text, Object obj) {
-                List<String> visited = (List<String>) obj;
-                visited.add(text.getClass().getName());
-                return visited;
-            }
-             
-        };
-        
-
-        
-        List<String> visited = (List<String>) cmnds.acceptVisitor(visitor, new ArrayList<String>());
-        List<String> expected = new ArrayList<String>(); {
-            expected.add(Commands.class.getName());
+        List<String> clzNames = classNames(cmnds);
+        List<String> expected = new ArrayList<String>();{
             expected.add(Paragraph.class.getName());
-            expected.add(Text.class.getName());
-            expected.add(Text.class.getName());
-            expected.add(Text.class.getName());
             expected.add(Line.class.getName());
             expected.add(cz.incad.kramerius.pdf.commands.List.class.getName());
-            expected.add(ListItem.class.getName());
-            expected.add(Text.class.getName());
-            expected.add(ListItem.class.getName());
-            expected.add(Text.class.getName());
             expected.add(Image.class.getName());
         }
+        Assert.assertEquals(expected, clzNames);
+
         
-        Assert.assertEquals(expected, visited);
-        
+        final List<String> processesed = new ArrayList<String>();
+        ITextCommandProcessListener listener = new ITextCommandProcessListener() {
+            
+            @Override
+            public void before(ITextCommand iTextCommand) {
+                processesed.add(iTextCommand.getClass().getName());
+            }
+
+            @Override
+            public void after(ITextCommand iTextCommand) {
+            }
+        };
+        cmnds.process(listener);
+
+        // top - down
+        expected = new ArrayList<String>();{
+            expected.add(ITextCommands.class.getName());
+            expected.add(Paragraph.class.getName());
+            expected.add(TextsArray.class.getName());
+            expected.add(Text.class.getName());
+            expected.add(Text.class.getName());
+            expected.add(Text.class.getName());
+
+            expected.add(Line.class.getName());
+
+            expected.add(cz.incad.kramerius.pdf.commands.List.class.getName());
+            expected.add(ListItem.class.getName());
+            expected.add(TextsArray.class.getName());
+            expected.add(Text.class.getName());
+
+            expected.add(ListItem.class.getName());
+            expected.add(TextsArray.class.getName());
+            expected.add(Text.class.getName());
+
+            expected.add(Image.class.getName());
+        }
+
+        Assert.assertEquals(expected, processesed);
+
+        processesed.clear();
+        listener = new ITextCommandProcessListener() {
+            
+            @Override
+            public void before(ITextCommand iTextCommand) {
+            }
+
+            @Override
+            public void after(ITextCommand iTextCommand) {
+                processesed.add(iTextCommand.getClass().getName());
+            }
+        };
+        cmnds.process(listener);
+
+        // bottom - up
+        expected = new ArrayList<String>();{
+            expected.add(Text.class.getName());
+            expected.add(Text.class.getName());
+            expected.add(Text.class.getName());
+            expected.add(TextsArray.class.getName());
+            expected.add(Paragraph.class.getName());
+
+            expected.add(Line.class.getName());
+
+            expected.add(Text.class.getName());
+            expected.add(TextsArray.class.getName());
+            expected.add(ListItem.class.getName());
+
+            expected.add(Text.class.getName());
+            expected.add(TextsArray.class.getName());
+            expected.add(ListItem.class.getName());
+
+            expected.add(cz.incad.kramerius.pdf.commands.List.class.getName());
+
+            expected.add(Image.class.getName());
+
+            expected.add(ITextCommands.class.getName());
+        }
+        Assert.assertEquals(expected, processesed);
+
+    }
+
+
+    public List<String> classNames(ITextCommands cmnds) {
+        List<String> list = new ArrayList<String>();
+        for (ITextCommand cmd : cmnds.getCommands()) {
+            list.add(cmd.getClass().getName());
+        }
+        return list;
     }
 }
