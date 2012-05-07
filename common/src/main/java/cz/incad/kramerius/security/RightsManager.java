eb7c7a785223ba4c6322641ceec6613e0e378fb9
pavel-stastny <pavel.stastny@gmail.com>
2012-05-07 13:26:54 +0000

    comments

--- common/src/main/java/cz/incad/kramerius/security/RightsManager.java
+++ common/src/main/java/cz/incad/kramerius/security/RightsManager.java
@@ -21,49 +21,151 @@ import java.util.List;
 
 import cz.incad.kramerius.ObjectPidsPath;
 
+/** 
+ * DAO objekt pro spravu prav systemu K4
+ * @author pavels
+ */
 public interface RightsManager {
     
     // najde prava pro uzivatele
-    public Right[] findRights(String[] uuids, String action, User user);
+    /**
+     * Najde prava vztazena na objekty (definovane pidy), akci a daneho uzivatele
+     * @param pids Objekty pro ktere hledame prava
+     * @param action Chranena akce
+     * @param user Uzivatel
+     */
+    public Right[] findRights(String[] pids, String action, User user);
 
     // interpretuje prava
-    public EvaluatingResult resolve(RightCriteriumContext ctx, String uuid, ObjectPidsPath path, String action, User user) throws RightCriteriumException;
+    /**
+     * Interpretace prav nad danym objektem
+     * @param ctx Kontext pro interpretaci prav
+     * @param pid PID objektu
+     * @param path Cesta k objektu
+     * @param action Chranena akce
+     * @param user Uzivatel
+     */
+    public EvaluatingResult resolve(RightCriteriumContext ctx, String pid, ObjectPidsPath path, String action, User user) throws RightCriteriumException;
     
     // interpretuje prava skrz celou cestu.  Od listu az ke korenu.
-    public EvaluatingResult[] resolveAllPath(RightCriteriumContext ctx, String uuid, ObjectPidsPath path, String action, User user) throws RightCriteriumException;
+    /**
+     * Interpretace prav pro vsechny objekty v ceste (od listu az ke korenu)
+     * @param ctx Kontext pro interpretaci prav
+     * @param pid PID objektu
+     * @param path Cesta k objektu
+     * @param action Chranena akce
+     * @param user Uzivatel
+     */
+    public EvaluatingResult[] resolveAllPath(RightCriteriumContext ctx, String pid, ObjectPidsPath path, String action, User user) throws RightCriteriumException;
 
     
     // najde prava pro skupinu
-    public Right[] findRightsForGroup(final String[] pids, final String action, final Role group);
+    /**
+     * Najde vsechna prava definovana pro objekty, chranenou akci a roli
+     * @param pids PIDy objektu
+     * @param action Chranena akce
+     * @param role Hledana role
+     */
+    public Right[] findRightsForGroup(final String[] pids, final String action, final Role role);
+
     // najde vsechna prava
-    public Right[] findAllRights(String[] uuids, String action);
+    /**
+     * Najde vsechna prava pro konkretni objekty a chranenou akci
+     * @param pids PIDy objektu
+     * @param action Chranena akce
+     */
+    public Right[] findAllRights(String[] pids, String action);
 
     
     // DAO methods - DAt to jinam !!
     // najde vsechny parametry 
+    /**
+     * Najde vsechny parametry
+     */
     public RightCriteriumParams[] findAllParams();
 
+    /**
+     * Najde parametr pro dle zadaneho id
+     * @param paramId Identifikator parametru
+     * @return
+     */
     public RightCriteriumParams findParamById(int paramId);
 
+    /**
+     * Find criterium by id 
+     * @param critId
+     * @return
+     */
     public RightCriteriumWrapper findRightCriteriumById(int critId);
     
+        
     public List<String> saturatePathAndCreatesPIDs(String uuid, String[] path);
 
+    /**
+     * Vlozit nove pravo
+     * @param right Nove pravo
+     * @return Vraci id nove zalozeneho prava
+     * @throws SQLException
+     */
     public int insertRight(Right right) throws SQLException;
 
+    /**
+     * Upravi existujici pravo
+     * @param right Upravovane pravo
+     * @throws SQLException
+     */
     public void updateRight(Right right) throws SQLException;
 
+    
+    /**
+     * Vytvori nove kriterium
+     * @param criterium Nove vkladane kriterium
+     * @return Vraci id nove vlozeneho kriteria
+     * @throws SQLException
+     */
     public int insertRightCriterium(RightCriteriumWrapper criterium) throws SQLException;
 
+    /**
+     * Upravi kriterium
+     * @param criterium Upravovavane kriterium
+     * @throws SQLException
+     */
     public void updateRightCriterium(RightCriteriumWrapper criterium) throws SQLException;
 
+    /**
+     * Vlozi nove parametry
+     * @param criteriumParams Nove vkladane parametry
+     * @return Vraci id nove vlozenych parametru
+     * @throws SQLException
+     */
     public int insertRightCriteriumParams(RightCriteriumParams criteriumParams) throws SQLException;
 
+    /**
+     * Upravi parametry kriteria
+     * @param criteriumParams Upravovane parametry
+     * @throws SQLException
+     */
     public void updateRightCriteriumParams(RightCriteriumParams criteriumParams) throws SQLException;
 
+    /**
+     * Smaze pravo 
+     * @param right Mazane pravo
+     * @throws SQLException
+     */
     public void deleteRight(Right right) throws SQLException;
     
+    
+    /**
+     * Najde pravo dle id
+     * @param id Identifikator prava
+     * @return
+     */
     public Right findRightById(int id);
     
+    
+    /**
+     * Najde vsechny pouzivane identifikatory roli
+     * @return
+     */
     public int[] findUsedRoleIDs();
 }
