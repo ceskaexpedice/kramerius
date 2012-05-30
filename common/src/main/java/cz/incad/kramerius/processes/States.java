3c5a95319aa7613e228846c5f956b6b2feb491eb
pavel-stastny <pavel.stastny@gmail.com>
2012-05-30 08:16:14 +0000

    comment

--- common/src/main/java/cz/incad/kramerius/processes/States.java
+++ common/src/main/java/cz/incad/kramerius/processes/States.java
@@ -6,95 +6,135 @@ import cz.incad.kramerius.processes.utils.ProcessUtils;
 
 /**
  * Processes states
+ * 
  * @author pavels
  */
 public enum States {
-	
-    
+
     /**
-     * Not running state = process just has been created
+     * Not running process
      */
-	NOT_RUNNING(0), 
-	
-	/**
-	 * Running proces
-	 */
-	RUNNING(1), 
-	
-	/**
-	 * Correct finished proces
-	 */
-	FINISHED(2), 
-	
-	/**
-	 * FAiled with some errors
-	 */
-	FAILED(3), 
-	
-	/**
-	 * Killed process
-	 */
-	KILLED(4),
-	
-	/**
-	 * Planned = process has been created and waiting to start
-	 */
-	PLANNED(5),
-	
-	
+    NOT_RUNNING(0),
+
+    /**
+     * Running proces
+     */
+    RUNNING(1),
+
+    /**
+     * Correct finished proces
+     */
+    FINISHED(2),
+
+    /**
+     * FAiled with some errors
+     */
+    FAILED(3),
+
+    /**
+     * Killed process
+     */
+    KILLED(4),
+
+    /**
+     * Planned (process is waiting to start)
+     */
+    PLANNED(5),
+
+    /**
+     * Batch process started (contains child processes and all of them are
+     * PLANNED or RUNNING).
+     */
     BATCH_STARTED(6),
-    
+
+    /**
+     * Batch process failed (some of child process FAILED)
+     */
     BATCH_FAILED(7),
-    
+
+    /**
+     * Batch process finished (all process finished with state FINISH)
+     */
     BATCH_FINISHED(8);
 
-	
-	public static States load(int v) {
-		for (States st : States.values()) {
-			if (st.getVal() == v) return st;
-		}
-		return null;
-	}
-	
-	private int val;
+    /**
+     * Load state from value
+     * @param v Given value of state
+     * @return
+     */
+    public static States load(int v) {
+        for (States st : States.values()) {
+            if (st.getVal() == v)
+                return st;
+        }
+        return null;
+    }
 
-	private States(int val) {
-		this.val = val;
-	}
+    private int val;
 
-	public int getVal() {
-		return val;
-	}
+    private States(int val) {
+        this.val = val;
+    }
 
+    public int getVal() {
+        return val;
+    }
+
+    /**
+     * Calculate master batch process state
+     * @param childStates Child process states
+     * @return
+     */
     public static States calculateBatchState(List<States> childStates) {
-        // ve stavu planned nebo running 
+        // ve stavu planned nebo running
         if (one(childStates, FAILED)) {
             return BATCH_FAILED;
         } else {
-            if (one(childStates,PLANNED, RUNNING)) {
+            if (one(childStates, PLANNED, RUNNING)) {
                 return BATCH_STARTED;
             }
             return BATCH_FINISHED;
         }
     }
 
+    /**
+     * Returns true if one of given childStates contains any expecting state
+     * @param childStates Child states
+     * @param exp Expecting states
+     * @return
+     */
     public static boolean one(List<States> childStates, States... exp) {
         for (States st : childStates) {
-            if (expect(st, exp)) return true; 
+            if (expect(st, exp))
+                return true;
         }
         return false;
     }
 
-    public static boolean all(List<States> childStates, States ... exp) {
+    /**
+     * Returns true if all of given child states contains any expecting state
+     * @param childStates Child states
+     * @param exp Execting state
+     * @return
+     */
+    public static boolean all(List<States> childStates, States... exp) {
         for (States st : childStates) {
-            if (!expect(st, exp)) return false; 
+            if (!expect(st, exp))
+                return false;
         }
         return true;
     }
 
-    public static boolean expect(States real, States ... expected) {
+    /**
+     * Returns true if given real state is one of expecting state
+     * @param real Real state
+     * @param expected Expecting states
+     * @return
+     */
+    public static boolean expect(States real, States... expected) {
         for (States exp : expected) {
-            if (real.equals(exp)) return true;
+            if (real.equals(exp))
+                return true;
         }
         return false;
     }
