67e5ae8a66f5c16b4ceba2857e2b63f03074c03f
pavel-stastny <pavel.stastny@gmail.com>
2012-06-05 21:01:41 +0000

    header

--- common/src/main/java/cz/incad/kramerius/security/SecuredFedoraAccessImpl.java
+++ common/src/main/java/cz/incad/kramerius/security/SecuredFedoraAccessImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012 Pavel Stastny
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package cz.incad.kramerius.security;
 
 import java.io.File;
@@ -32,16 +48,7 @@ import cz.incad.kramerius.utils.conf.KConfiguration;
 
 /**
  * This is secured variant of class FedoraAccessImpl {@link FedoraAccessImpl}. <br>
- * Only three methos are secured:
- * <ul>
- * <li>FedoraAccess#getImageFULL(String)</li>
- * <li>FedoraAccess#isImageFULLAvailable(String)</li>
- * <li>FedoraAccess#getImageFULLMimeType(String)</li>
- * </ul>
  * 
- * @see FedoraAccess#getImageFULL(String)
- * @see FedoraAccess#isImageFULLAvailable(String)
- * @see FedoraAccess#getImageFULLMimeType(String)
  * @author pavels
  */
 public class SecuredFedoraAccessImpl implements FedoraAccess {
