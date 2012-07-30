8dfd4c73a6c66eb95dbc82b8e52a678d8af984d3
pavel-stastny <pavel.stastny@gmail.com>
2012-07-30 07:08:11 +0000

    Issue 114 update issue 114

---
+++ common/src/main/java/cz/incad/kramerius/service/processes/K4ReplicationProcess.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2012 Pavel Stastny
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.incad.kramerius.service.processes;
+
+import cz.incad.kramerius.processes.annotations.ParameterName;
+import cz.incad.kramerius.processes.annotations.Process;
+import cz.incad.kramerius.service.ReplicationService;
+
+public class K4ReplicationProcess {
+
+    ReplicationService replicationService;
+    
+    
+    @Process
+    public static void process(@ParameterName("handle") String handle) {
+        System.out.println("Handle it :"+handle);
+    }
+}
