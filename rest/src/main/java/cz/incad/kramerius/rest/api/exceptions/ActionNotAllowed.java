2f5726c85da24e82808e4abacc20f248d149c2e5
pavel-stastny <pavel.stastny@gmail.com>
2012-10-05 09:09:41 +0000

    Issue 445 update  issue 445

---
+++ rest/src/main/java/cz/incad/kramerius/rest/api/exceptions/ActionNotAllowed.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2012 Pavel Stastny
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.incad.kramerius.rest.api.exceptions;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import cz.incad.kramerius.rest.api.processes.exceptions.ExceptionMessageBuilder;
+
+/**
+ * User is not permitted to permorm action
+ * @author pavels
+ */
+public class ActionNotAllowed extends WebApplicationException{
+
+    public ActionNotAllowed(String message) {
+        super(Response.status(HttpServletResponse.SC_FORBIDDEN).entity(ExceptionMessageBuilder.createErrorMessage(401,message)).type(MediaType.APPLICATION_JSON).build());
+    }
+}
