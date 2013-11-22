46874f263e6bf5cabcf30b6272b8d404d3f95df4
pavel-stastny <pavel.stastny@gmail.com>
2013-11-22 09:08:12 +0000

    authentication resource

---
+++ rest/src/main/java/cz/incad/kramerius/rest/api/k5/client/authentication/AuthenticationResource.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2013 Pavel Stastny
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.incad.kramerius.rest.api.k5.client.authentication;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import net.sf.json.JSONObject;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import cz.incad.kramerius.security.User;
+
+@Path("/auth")
+public class AuthenticationResource {
+
+	@Inject
+	Provider<User> provider;
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response auth() {
+		User user = provider.get();
+		if (user != null) {
+			JSONObject jsonObj = new JSONObject();
+			jsonObj.put("loginname", user.getLoginname());
+			jsonObj.put("firstname", user.getFirstName());
+			jsonObj.put("surname", user.getSurname());
+			return Response.ok().entity(jsonObj.toString()).build();
+		} else {
+			return Response.ok().entity(new JSONObject().toString()).build();
+		}
+	}
+}
