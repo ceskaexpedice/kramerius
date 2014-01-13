eaf46d9beda9add3ee259b9f8483f3b721f006ca
pavel.stastny@gmail.com <pavel.stastny@gmail.com>
2014-01-13 21:21:11 +0100

    api changes

---
+++ rest/src/main/java/cz/incad/kramerius/rest/api/client/RightsClient.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2013 Pavel Stastny
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.incad.kramerius.rest.api.client;
+
+import javax.ws.rs.core.MediaType;
+
+import net.sf.json.JSONObject;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+
+public class RightsClient {
+	
+	public static final String DEFAULT_NAME="k4_admin";
+	public static final String DEFAULT_PSWD="k4_admin";
+	
+    public static String deleteRight(String delId) {
+    	Client c = Client.create();
+        WebResource r = c.resource("http://localhost:8080/search/k5/admin/rights/"+delId);
+        r.addFilter(new BasicAuthenticationFilter(DEFAULT_NAME, DEFAULT_PSWD));
+        String t = r.accept(MediaType.APPLICATION_JSON).delete(String.class);
+        return t;
+    }
+    
+    public static String createRight(JSONObject jsonObj) {
+    	Client c = Client.create();
+        WebResource r = c.resource("http://localhost:8080/search/api/v4.6/k5/admin/rights");
+        r.addFilter(new BasicAuthenticationFilter(DEFAULT_NAME, DEFAULT_PSWD));
+        String t = r.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON).post(String.class);
+        return t;
+    }
+    
+    public static String rights() {
+    	Client c = Client.create();
+        WebResource r = c.resource("http://localhost:8080/search/api/v4.6/k5/admin/rights");
+        r.addFilter(new BasicAuthenticationFilter(DEFAULT_NAME, DEFAULT_PSWD));
+        String t = r.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON).get(String.class);
+        return t;
+    }
+
+    public static String right(String id) {
+    	Client c = Client.create();
+        WebResource r = c.resource("http://localhost:8080/search/api/v4.6/k5/admin/rights/"+id);
+        r.addFilter(new BasicAuthenticationFilter(DEFAULT_NAME, DEFAULT_PSWD));
+        String t = r.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON).get(String.class);
+        return t;
+    }
+
+    public static String params() {
+    	Client c = Client.create();
+        WebResource r = c.resource("http://localhost:8080/search/api/v4.6/k5/admin/rights/params");
+        r.addFilter(new BasicAuthenticationFilter(DEFAULT_NAME, DEFAULT_PSWD));
+        String t = r.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON).get(String.class);
+        return t;
+    }
+
+    public static String param(String paramId) {
+    	Client c = Client.create();
+        WebResource r = c.resource("http://localhost:8080/search/api/v4.6/k5/admin/rights/params/"+paramId);
+        r.addFilter(new BasicAuthenticationFilter(DEFAULT_NAME, DEFAULT_PSWD));
+        String t = r.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON).get(String.class);
+        return t;
+    }
+
+    public static String deleteParam(String paramId) {
+    	Client c = Client.create();
+        WebResource r = c.resource("http://localhost:8080/search/api/v4.6/k5/admin/rights/params/"+paramId);
+        r.addFilter(new BasicAuthenticationFilter(DEFAULT_NAME, DEFAULT_PSWD));
+        String t = r.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON).get(String.class);
+        return t;
+    }
+
+    private static String createSampleRight() {
+		JSONObject jsonObj = new JSONObject();
+		jsonObj.put("action", "read");
+		jsonObj.put("pid", "uuid:1");
+		jsonObj.put("role", JSONObject.fromObject(UsersClient.role("3")));
+
+    	Client c = Client.create();
+        WebResource r = c.resource("http://localhost:8080/search/api/v4.6/k5/admin/rights");
+        r.addFilter(new BasicAuthenticationFilter(DEFAULT_NAME, DEFAULT_PSWD));
+        String t = r.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON).entity(jsonObj.toString()).post(String.class);
+        return t;
+    }
+
+    
+    public static void main(String[] args) {
+//		String rights = rights();
+//		System.out.println(rights);
+    	
+    	System.out.println(right("45"));
+    	System.out.println(deleteRight("45"));
+    	System.out.println(right("45"));
+
+		
+    }
+
+    
+}
---
+++ rest/src/main/java/cz/incad/kramerius/rest/api/exceptions/GenericApplicationException.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2013 Pavel Stastny
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.incad.kramerius.rest.api.exceptions;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class GenericApplicationException extends AbstractRestJSONException{
+
+	public GenericApplicationException(String message, Exception ex) {
+		super(message, ex, HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	}
+
+	public GenericApplicationException(String message) {
+		super(message, HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	}
+	
+}
