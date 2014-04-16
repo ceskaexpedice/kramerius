b9d4a8baeabc1866ae8a2d4c7c6de1dee8832b60
pavel.stastny@gmail.com <pavel.stastny@gmail.com>
2014-04-16 23:40:14 +0200

    IOException & remove key

--- common/src/main/java/cz/incad/kramerius/pdf/impl/GeneratePDFServiceImpl.java
+++ common/src/main/java/cz/incad/kramerius/pdf/impl/GeneratePDFServiceImpl.java
@@ -212,18 +212,25 @@ public class GeneratePDFServiceImpl extends AbstractPDFRenderSupport implements 
 
         } catch (DocumentException e) {
             LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            throw new IOException(e.getMessage());
         } catch (XPathExpressionException e) {
             LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            throw new IOException(e.getMessage());
         } catch (TransformerException e) {
             LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            throw new IOException(e.getMessage());
         } catch (InstantiationException e) {
             LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            throw new IOException(e.getMessage());
         } catch (IllegalAccessException e) {
             LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            throw new IOException(e.getMessage());
         } catch (ParserConfigurationException e) {
             LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            throw new IOException(e.getMessage());
         } catch (SAXException e) {
             LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            throw new IOException(e.getMessage());
         }
         return rdoc;
     }
