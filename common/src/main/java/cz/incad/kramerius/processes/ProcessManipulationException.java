50dbd6ff8abb945fa92c2f8215141fc88e65a442
pavel.stastny@gmail.com <pavel.stastny@gmail.com>
2014-07-08 13:43:18 +0200

    Remove java 1.7 dependency

--- common/src/main/java/cz/incad/kramerius/processes/ProcessManipulationException.java
+++ common/src/main/java/cz/incad/kramerius/processes/ProcessManipulationException.java
@@ -6,10 +6,11 @@ public class ProcessManipulationException extends Exception {
         super();
     }
 
+    /*
     public ProcessManipulationException(String message, Throwable cause,
             boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
-    }
+    }*/
 
     public ProcessManipulationException(String message, Throwable cause) {
         super(message, cause);
