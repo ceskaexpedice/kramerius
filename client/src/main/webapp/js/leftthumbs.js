2ed03a5812d61094acf3dd5c050791908f313b10
alberto <alberto@albertoX1>
2015-12-03 15:50:26 +0100

    opravy podle "pripominky.docx"

--- client/src/main/webapp/js/leftthumbs.js
+++ client/src/main/webapp/js/leftthumbs.js
@@ -176,11 +176,11 @@ LeftThumbs.prototype = {
                 $(this).tooltip("option", "content", $(this).data("tt"));
             });
             $('td.thumb').removeClass("hit chit");
-            this.getHits();
+            this.getHits(true);
         }
         cleanWindow();
     },
-    getHits: function() {
+    getHits: function(showalert) {
         if ($("#q").val() === "") {
             return;
         }
@@ -195,11 +195,24 @@ LeftThumbs.prototype = {
             var q = "q=" + $("#q").val() + "&rows=5000&fq=pid_path:" + pid_path.replace(/:/g, "\\:") + "*";
             var hl = "&hl=true&hl.fl=text_ocr&hl.mergeContiguous=true&hl.snippets=2";
             K5.api.askForSolr(q + hl, _.bind(function(data) {
+                var numFound = data.response.numFound;
                 console.log("Hits: " + data.response.numFound);
                 //console.log(JSON.stringify(data));
                 this.hits = data.response.docs;
                 this.highlighting = data.highlighting;
                 this.setHitClass();
+                
+                if(showalert){
+                    var key = 'common.page.plural_2';
+                    if (numFound > 4) {
+                        key = 'common.page.plural_2';
+                    } else if (numFound > 1) {
+                        key = 'common.page.plural_1';
+                    } else {
+                        key = 'common.page.singural';
+                    }
+                    alert(K5.i18n.ctx.dictionary["common.found"] + " " + numFound + " " + K5.i18n.ctx.dictionary[key]);
+                }
             }, this));
         } else {
             this.setHitClass();
