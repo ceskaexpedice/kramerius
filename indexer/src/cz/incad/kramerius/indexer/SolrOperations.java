f0ab14ff9db6825e4ac9b5940fccf7eccc3a572a
Pavel Stastny <pstastny@searchtechnologies.com>
2017-11-07 10:14:41 +0100

    Issues #436 &  #236

--- indexer/src/cz/incad/kramerius/indexer/SolrOperations.java
+++ indexer/src/cz/incad/kramerius/indexer/SolrOperations.java
@@ -544,18 +544,21 @@ public class SolrOperations {
         }
     }
 
-    
-    public static String prepareDocForIndexing(String rawXML) throws ParserConfigurationException, SAXException, IOException,
+    public static String prepareDocForIndexing(boolean compositeId, String rawXML) throws ParserConfigurationException, SAXException, IOException,
             TransformerException, UnsupportedEncodingException {
         Document document = XMLUtils.parseDocument(new StringReader(rawXML));
         Element docroot = document.getDocumentElement();
-        boolean compsoiteId  = KConfiguration.getInstance().getConfiguration().getBoolean("indexer.compositeId", false);
-        if (compsoiteId) {
+        if (compositeId) {
             PrepareIndexDocUtils.enhanceByCompositeId(document, document.getDocumentElement());
         }
         rawXML = PrepareIndexDocUtils.wrapByAddCommand(document);
         String docSrc = removeTroublesomeCharacters(rawXML);
         return docSrc;
+    }
+
+    public static String prepareDocForIndexing(String rawXML) throws ParserConfigurationException, SAXException, IOException,
+            TransformerException, UnsupportedEncodingException {
+        return prepareDocForIndexing(KConfiguration.getInstance().getConfiguration().getBoolean("indexer.compositeId", false), rawXML);
     }
 
     
--- indexer/src/cz/incad/kramerius/indexer/res/K4.xslt
+++ indexer/src/cz/incad/kramerius/indexer/res/K4.xslt
@@ -206,7 +206,7 @@
 
     
     <xsl:template match="/foxml:digitalObject/foxml:datastream[@ID='BIBLIO_MODS']/foxml:datastreamVersion[last()]/foxml:xmlContent/mods:modsCollection/mods:mods" mode="biblioMods">
-        <xsl:for-each select="mods:language[@objectPart != 'translation']/mods:languageTerm/text()">
+        <xsl:for-each select="mods:language[not(@objectPart) or @objectPart != 'translation']/mods:languageTerm/text()">
         <field name="language">
             <xsl:value-of select="." />
         </field>
