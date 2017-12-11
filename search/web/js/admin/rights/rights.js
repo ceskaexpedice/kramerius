35bbadb4fc65eb7198e48d20e9b47900a0e7e6e1
pavels <pavels@localhost.localdomain>
2017-12-11 16:00:23 +0100

    Last changes

--- search/web/js/admin/rights/rights.js
+++ search/web/js/admin/rights/rights.js
@@ -688,11 +688,11 @@ function GlobalActions() {
 // volano z hlavniho menu .
 GlobalActions.prototype.rigthsForAction=function(action,pid) {
 	// affected rights secured actions 
-	findObjectsDialog().securedActionTabs[action] = findObjectsDialog().createSecurityActionTab(action,"inc/admin/_display_rights_for_global_actions.jsp?pids={uuid\\:1}&securedaction="+action);
+	findObjectsDialog().securedActionTabs[action] = findObjectsDialog().createSecurityActionTab(action,"inc/admin/_display_rights_for_global_actions.jsp?pids="+encodeURIComponent("{uuid\\:1}")+"&securedaction="+action);
 	findObjectsDialog().securedActionTabs[action].retrieve = findObjectsDialog().securedActionTabs[action].retrieveGlobalContent;
 	
 	var pids = (pid ? pid : "{uuid\\:1}");
-	var url = "inc/admin/_display_rights_for_global_actions.jsp?pids="+pids+"&securedaction="+action;
+	var url = "inc/admin/_display_rights_for_global_actions.jsp?pids="+encodeURIComponent(pids)+"&securedaction="+action;
 	$.get(url, bind(function(data) {
 		if (this.actionDialog) {
 			this.actionDialog.dialog('open');
