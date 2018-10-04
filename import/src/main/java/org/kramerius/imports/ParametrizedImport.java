a3a0d753686d11042a5a33d69ce82d08c9247e33
Stastny <pavel.stastny@accenture.com>
2018-10-04 15:38:01 +0200

    Issue #598

--- import/src/main/java/org/kramerius/imports/ParametrizedImport.java
+++ import/src/main/java/org/kramerius/imports/ParametrizedImport.java
@@ -20,11 +20,15 @@ import cz.incad.kramerius.processes.annotations.ParameterName;
 import cz.incad.kramerius.processes.annotations.Process;
 import cz.incad.kramerius.processes.impl.ProcessStarter;
 
+import cz.incad.kramerius.utils.FedoraUtils;
+import cz.incad.kramerius.utils.XMLUtils;
+import org.apache.commons.io.FileUtils;
 import org.kramerius.Import;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
 import java.util.logging.Level;
 
 /**
@@ -41,21 +45,56 @@ public class ParametrizedImport {
     @Process
     public static void process( @ParameterName("importDirectory") File importDirectory, 
             @ParameterName("startIndexer")Boolean startIndexer,
-            @ParameterName("updateExisting")Boolean updateExisting) throws UnsupportedEncodingException {
+            @ParameterName("updateExisting")Boolean updateExisting,
+            @ParameterName("onlyReplaceDC")Boolean replaceDC,
+            @ParameterName("onlyReplaceMODS")Boolean replaceMODs) throws IOException, ParserConfigurationException, SAXException {
 
         System.setProperty("import.directory", importDirectory.getAbsolutePath());
         System.setProperty("ingest.startIndexer", startIndexer.toString());
-        System.setProperty("ingest.updateExisting", updateExisting.toString());
+        System.setProperty("ingest.updateExisting", updateExisting.toString()); // only RELS-EXT update
         System.setProperty("ingest.skip", "false");   //import se bude vždy spouštět
-        
+
+
         try {
             //TODO: I18N
             ProcessStarter.updateName("Parametrizovany import z '"+importDirectory.getAbsolutePath()+"'");
         } catch (IOException e) {
             LOGGER.log(Level.SEVERE,e.getMessage(), e);
         }
-        
+
+        // update metadata
+        if (replaceDC || replaceMODs) {
+            // prepare updatelist
+            prepareFile(importDirectory, replaceDC, replaceMODs);
+        }
+
         //standard import program
         Import.main(new String[0]);
+    }
+
+    private static final String DEFAULT_LIST_NAME = "update.list";
+    private static String prepareFile(File importDirectory, Boolean replaceDC, Boolean replaceMODs) throws IOException, SAXException, ParserConfigurationException {
+        StringBuilder builder = new StringBuilder();
+        File[] files = importDirectory.listFiles();
+        if (files != null) {
+            for (File f : files) {
+                Document document = XMLUtils.parseDocument(new FileInputStream(f), true);
+                String pid = document.getDocumentElement().getAttribute("PID");
+                builder.append(pid);
+                if (replaceDC) {
+                    builder.append(' ').append(FedoraUtils.DC_STREAM);
+                }
+                if (replaceMODs) {
+                    builder.append(' ').append(FedoraUtils.BIBLIO_MODS_STREAM);
+                }
+                builder.append('\n');
+            }
+        }
+        File f = new File(importDirectory, DEFAULT_LIST_NAME);
+        if (!f.exists()) {
+            f.createNewFile();
+        }
+        FileUtils.writeStringToFile(f, builder.toString());
+        return f.getAbsolutePath();
     }
 }
