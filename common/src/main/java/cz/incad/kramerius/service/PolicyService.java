f8b65ada9190b30f172bafdf46e8f727a4f34dbd
gmelingerova <gabrielamelingerova@seznam.cz>
2019-07-30 08:59:26 +0200

    Issue #691: adding option for changing flag on selected level

--- common/src/main/java/cz/incad/kramerius/service/PolicyService.java
+++ common/src/main/java/cz/incad/kramerius/service/PolicyService.java
@@ -11,5 +11,14 @@ public interface PolicyService {
      * @throws IOException 
      */
     public void setPolicy(String pid, String policyName) throws IOException;
+    
+    /**
+     * Set the given policy the tree of Kramerius objects including the root with given PID 
+     * @param pid PID of the tree root object
+     * @param policyName id of the policy to be set (currently "public" or "private")
+     * @param level if this process is only for the selected level
+     * @throws IOException 
+     */
+    public void setPolicy(String pid, String policyName, String level) throws IOException;
 
 }
