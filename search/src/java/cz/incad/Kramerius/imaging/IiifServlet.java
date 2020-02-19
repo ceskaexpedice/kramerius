b49df69aab46374fd2b56daaad99088e91afa905
vlahoda <vlahoda@gmail.com>
2020-02-19 12:57:06 +0100

    removed PID validation from IIFservlet Fixes #729

--- search/src/java/cz/incad/Kramerius/imaging/IiifServlet.java
+++ search/src/java/cz/incad/Kramerius/imaging/IiifServlet.java
@@ -63,10 +63,6 @@ public class IiifServlet extends AbstractImageServlet {
             //unescape PID
             pid = URLDecoder.decode(pid, "UTF-8");
 
-            if (!pid.matches("uuid:[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}")) {
-                resp.sendError(HttpServletResponse.SC_NOT_FOUND);
-                return;
-            }
 
             ObjectPidsPath[] paths = solrAccess.getPath(pid);
             boolean permited = false;
