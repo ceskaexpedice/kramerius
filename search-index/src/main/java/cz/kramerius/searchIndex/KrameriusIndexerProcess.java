68b2bcc4c053efeff803e9a60a15de0c80f271b6
Martin Řehánek <martin.rehanek@gmail.com>
2021-01-18 13:50:18 +0100

    Indexer optimization - indexer now uses low-level java access to repository instead of public http API, which introduced significant overhead. Processing/resource index access is still through http API.

--- search-index/src/main/java/cz/kramerius/searchIndex/KrameriusIndexerProcess.java
+++ search-index/src/main/java/cz/kramerius/searchIndex/KrameriusIndexerProcess.java
@@ -1,15 +1,23 @@
 package cz.kramerius.searchIndex;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.name.Names;
+import cz.incad.kramerius.fedora.RepoModule;
 import cz.incad.kramerius.processes.starter.ProcessStarter;
 import cz.incad.kramerius.processes.utils.Utils;
+import cz.incad.kramerius.resourceindex.ResourceIndexModule;
+import cz.incad.kramerius.solr.SolrModule;
+import cz.incad.kramerius.statistics.NullStatisticsModule;
 import cz.kramerius.adapters.FedoraAccess;
 import cz.kramerius.adapters.IResourceIndex;
 import cz.kramerius.searchIndex.indexer.SolrConfig;
 import cz.kramerius.searchIndex.indexerProcess.IndexationType;
 import cz.kramerius.searchIndex.indexerProcess.IndexerProcess;
 import cz.kramerius.searchIndex.repositoryAccess.KrameriusRepositoryAccessAdapter;
-import cz.kramerius.searchIndex.repositoryAccessImpl.krameriusNewApi.RepositoryAccessImplByKrameriusNewApis;
 import cz.kramerius.searchIndex.repositoryAccessImpl.krameriusNewApi.ResourceIndexImplByKrameriusNewApis;
+import cz.kramerius.searchIndex.repositoryAccessImpl.krameriusNoApi.RepositoryAccessImplByKrameriusDirect;
 
 import java.io.IOException;
 import java.util.logging.Logger;
@@ -48,21 +56,29 @@ public class KrameriusIndexerProcess {
 
         //zmena nazvu
         //TODO: mozna spis abstraktni proces s metodou updateName() a samotny kod procesu by mel callback na zjisteni nazvu, kterym by se zavolal updateName()
-        ProcessStarter.updateName(String.format("Indexace (typ %s, objekt %s)", type, pid));
+        ProcessStarter.updateName(String.format("Indexace (objekt %s, typ %s)", pid, type));
 
         SolrConfig solrConfig = new SolrConfig(solrBaseUrl, solrCollection, solrUseHttps, solrLogin, solrPassword);
         //TODO: merge KrameriusIndexerProcess and IndexerProcess
-        RepositoryAccessImplByKrameriusNewApis.Credentials krameriusCredentials = new RepositoryAccessImplByKrameriusNewApis.Credentials(krameriusApiAuthClient, krameriusApiAuthUid, krameriusApiAuthAccessToken);
-        FedoraAccess repository = new RepositoryAccessImplByKrameriusNewApis(krameriusBackendBaseUrl, krameriusCredentials);
+
+        //access to repository through new public APIs
+        /*RepositoryAccessImplByKrameriusNewApis.Credentials krameriusCredentials = new RepositoryAccessImplByKrameriusNewApis.Credentials(krameriusApiAuthClient, krameriusApiAuthUid, krameriusApiAuthAccessToken);
+        FedoraAccess repository = new RepositoryAccessImplByKrameriusNewApis(krameriusBackendBaseUrl, krameriusCredentials);*/
+
+        //access to repository through java directly (injected cz.incad.kramerius.FedoraAccess)
+        Injector injector = Guice.createInjector(new SolrModule(), new ResourceIndexModule(), new RepoModule(), new NullStatisticsModule());
+        cz.incad.kramerius.FedoraAccess rawRepositoryAccess = injector.getInstance(Key.get(cz.incad.kramerius.FedoraAccess.class, Names.named("rawFedoraAccess")));
+        FedoraAccess repository = new RepositoryAccessImplByKrameriusDirect(rawRepositoryAccess);
+
+        //access to resource index through new public APIs
         IResourceIndex resourceIndex = new ResourceIndexImplByKrameriusNewApis(krameriusBackendBaseUrl);
-        //TODO: smazat druhy KrameriusRepositoryAccessAdapter
+
         KrameriusRepositoryAccessAdapter repositoryAdapter = new KrameriusRepositoryAccessAdapter(repository, resourceIndex);
         IndexerProcess process = new IndexerProcess(repositoryAdapter, solrConfig, System.out);
         process.indexByObjectPid(pid, IndexationType.valueOf(type));
         LOGGER.info("Indexation finished");
 
         //TODO: zmenit nazev procesu - doplnit nazev objektu (bud odsud, nebo lepe callbackem z procesu)
-
         LOGGER.info("total duration: " + Utils.formatTime(System.currentTimeMillis() - start));
     }
 }
