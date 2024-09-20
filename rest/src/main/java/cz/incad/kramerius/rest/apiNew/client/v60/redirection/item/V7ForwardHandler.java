cb2b97ea70921486927e982f0ad506a90a5dd8f6
pavel-stastny <pavel.stastny@gmail.com>
2024-09-20 05:48:33 +0200

    Fix alto stream

--- rest/src/main/java/cz/incad/kramerius/rest/apiNew/client/v60/redirection/item/V7ForwardHandler.java
+++ rest/src/main/java/cz/incad/kramerius/rest/apiNew/client/v60/redirection/item/V7ForwardHandler.java
@@ -167,13 +167,12 @@ public class V7ForwardHandler extends V7RedirectHandler {
 
     @Override
     public Response altoOCR(RequestMethodName method) throws ProxyHandlerException {
-        String baseurl = super.baseUrl();
+        String baseurl = this.forwardUrl();
         String url = baseurl + (baseurl.endsWith("/") ? "" : "/") + "api/cdk/v7.0/forward/item/" + this.pid
                 + "/streams/ALTO";
         if (method == RequestMethodName.head) {
             return buildForwardResponseHEAD(url);
         } else {
-            LOGGER.info(String.format("ALTO OCR url, pid, deleteTrigger = %s,%s, %b", (url != null ? url : "null"), "",false));
             return buildForwardResponseGET(url, null,false);
         }
     }
