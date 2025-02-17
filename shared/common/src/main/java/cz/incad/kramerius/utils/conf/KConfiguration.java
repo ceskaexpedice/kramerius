f74a59ee44a98f6a3579e5fb6455e1fbcb62dc71
SPXcz <ondrachu@gmail.com>
2025-02-17 11:54:11 +0100

    fix(KConfiguration): fix synchronized bottleneck

--- shared/common/src/main/java/cz/incad/kramerius/utils/conf/KConfiguration.java
+++ shared/common/src/main/java/cz/incad/kramerius/utils/conf/KConfiguration.java
@@ -32,16 +32,21 @@ public class KConfiguration {
         workingDir = newWorkingDir;
     }
 
-    public synchronized static KConfiguration getInstance() {
-        if (_sharedInstance == null) {
-            if (workingDir == null){
-                workingDir = WORKING_DIR;
-            }
-            _sharedInstance = new KConfiguration(workingDir);
+    public static KConfiguration getInstance() {
+        if (_sharedInstance == null || workingDir == null) {
+            setUpInstance();
         }
         return _sharedInstance;
     }
 
+    public static synchronized void setUpInstance() {
+        if (_sharedInstance == null) {
+            if (workingDir == null) {
+                workingDir = WORKING_DIR;
+            }
+            _sharedInstance = new KConfiguration(workingDir);
+        }
+    }
 
     private Configuration allConfigurations;
     private String configDir = null;
