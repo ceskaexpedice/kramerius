a463c335a84f36b4ffabc6c4a00e76f742d16903
pavel-stastny <pavel.stastny@gmail.com>
2025-05-27 09:39:15 +0200

    Issue #1110

--- shared/common/src/main/java/cz/incad/kramerius/fedora/AkubraRepositoryProvider.java
+++ shared/common/src/main/java/cz/incad/kramerius/fedora/AkubraRepositoryProvider.java
@@ -1,6 +1,11 @@
 package cz.incad.kramerius.fedora;
 
+import com.google.common.base.Functions;
+import com.google.common.collect.Lists;
 import com.google.inject.Provider;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
 import cz.incad.kramerius.utils.conf.KConfiguration;
 import org.ceskaexpedice.akubra.AkubraRepository;
 import org.ceskaexpedice.akubra.AkubraRepositoryFactory;
@@ -8,6 +13,9 @@ import org.ceskaexpedice.akubra.config.RepositoryConfiguration;
 import org.ceskaexpedice.hazelcast.HazelcastConfiguration;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class AkubraRepositoryProvider implements Provider<AkubraRepository> {
 
@@ -27,11 +35,16 @@ public class AkubraRepositoryProvider implements Provider<AkubraRepository> {
         String hazelcastInstance = KConfiguration.getInstance().getConfiguration().getString("hazelcast.instance");
         String hazelcastUser = KConfiguration.getInstance().getConfiguration().getString("hazelcast.user");
 
+        List<String> address = Lists.transform(KConfiguration.getInstance().getConfiguration().getList("hazelcast.server.addresses", new ArrayList<>()), Functions.toStringFunction());
         HazelcastConfiguration hazelcastConfig = new HazelcastConfiguration.Builder()
                 .hazelcastClientConfigFile(hazelcastConfigFileS)
                 .hazelcastInstance(hazelcastInstance)
+                .setHazelcastServers(address.toArray(new String[0]))
                 .hazelcastUser(hazelcastUser)
                 .build();
+
+
+
         RepositoryConfiguration config = new RepositoryConfiguration.Builder()
                 .processingIndexHost(solrProcessingHost)
                 .objectStorePath(objectPath)
@@ -40,6 +53,7 @@ public class AkubraRepositoryProvider implements Provider<AkubraRepository> {
                 .datastreamStorePattern(datastreamStorePattern)
                 .hazelcastConfiguration(hazelcastConfig)
                 .build();
+
         AkubraRepository akubraRepository = AkubraRepositoryFactory.createRepository(config);
         return akubraRepository;
     }
