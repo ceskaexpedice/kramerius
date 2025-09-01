3e7431b6683484e1fbbe55a52eb8e887880e4983
pavel-stastny <pavel.stastny@gmail.com>
2025-09-01 14:56:08 +0200

    First attempt at a woker node function

---
+++ nprocesses/test/mock/src/main/java/org/ceskaexpedice/kramerius/processes/MockProcess.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2025  Inovatika
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.ceskaexpedice.kramerius.processes;
+
+
+import cz.incad.kramerius.processes.mock.MockLPProcess;
+import org.ceskaexpedice.processplatform.api.annotations.ProcessMethod;
+
+import java.io.IOException;
+
+public class MockProcess {
+
+    @ProcessMethod
+    public static void test() throws IOException {
+        MockLPProcess.main(new String[]{});
+    }
+}
