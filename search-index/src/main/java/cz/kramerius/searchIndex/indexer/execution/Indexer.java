3deb4c643561f21ff0c4c3c6b904849d5e6c6fcd
pavel-stastny <pavel.stastny@gmail.com>
2025-12-18 07:55:50 +0100

    Changed indexer version

--- search-index/src/main/java/cz/kramerius/searchIndex/indexer/execution/Indexer.java
+++ search-index/src/main/java/cz/kramerius/searchIndex/indexer/execution/Indexer.java
@@ -31,7 +31,8 @@ import java.util.logging.Logger;
 public class Indexer {
     private static final Logger LOGGER = Logger.getLogger(Indexer.class.getName());
 
-    public static final int INDEXER_VERSION = 21; //this should be updated after every change in logic, that affects full indexation
+    //public static final int INDEXER_VERSION = 21; //this should be updated after every change in logic, that affects full indexation
+    public static final int INDEXER_VERSION = 22; //this should be updated after every change in logic, that affects full indexation
 
     private final SolrConfig solrConfig;
     //only state variable
