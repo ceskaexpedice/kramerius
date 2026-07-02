a7decc6f27d5b0d406d9167fb3d48b7a6204f6a6
pavel-stastny <pavel.stastny@gmail.com>
2026-07-02 17:19:54 +0200

    Issue #1245

--- processes/search-index/src/main/java/cz/kramerius/searchIndex/indexer/execution/Indexer.java
+++ processes/search-index/src/main/java/cz/kramerius/searchIndex/indexer/execution/Indexer.java
@@ -288,7 +288,7 @@ public class Indexer {
                 try {
                     SolrInput solrInput = solrInputBuilder.processObjectFromRepository(akubraRepository, foxmlDoc, ocrText, repositoryNode, nodeManager, imgFullMime, audioLength, setFullIndexationInProgress);
                     String solrInputStr = solrInput.getDocument().asXML();
-                    LOGGER.info("Indexing " + pid + " with input: " + solrInputStr);
+                    LOGGER.fine("Indexing " + pid + " with input: " + solrInputStr);
                     solrIndexer.indexFromXmlString(solrInputStr, false);
                 } catch (DocumentException e) {  //try to reindex without ocr - TODO: hack, ocr should be properly escaped
                     //typical root cause: Caused by: org.xml.sax.SAXParseException; lineNumber: 2; columnNumber: 2302; Character reference "&#6" is an invalid XML character.
@@ -352,7 +352,7 @@ public class Indexer {
             String ocrText = normalizeWhitespacesForOcrText(extractor.getPageText(i));
             SolrInput solrInput = solrInputBuilder.processPageFromPdf(nodeManager, repositoryNode, pageNumber, ocrText);
             String solrInputStr = solrInput.getDocument().asXML();
-            LOGGER.info("Indexing " + pid + " page " + pageNumber + " with input: " + solrInputStr);
+            LOGGER.fine("Indexing " + pid + " page " + pageNumber + " with input: " + solrInputStr);
             solrIndexer.indexFromXmlString(solrInputStr, false);
             counters.incrementIndexed();
             report("");
