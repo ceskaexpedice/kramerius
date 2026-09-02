dfd70dc33269ee938f92ab984ed3bbd09284b457
ppodsednik <petr.podsednik@gmail.com>
2026-09-02 15:34:45 +0200

    fix parameters of cdk-delete-library

--- processes/platform/cdk/cdk-delete-library/src/main/java/cz/incad/kramerius/plugin/CDKDeleteLibraryProcess.java
+++ processes/platform/cdk/cdk-delete-library/src/main/java/cz/incad/kramerius/plugin/CDKDeleteLibraryProcess.java
@@ -15,8 +15,8 @@ public class CDKDeleteLibraryProcess {
             @ParameterName("destinationUrl") @IsRequired String destinationUrl,
             @ParameterName("library") @IsRequired String library,
             @ParameterName("filterQuery") String filterQuery,
-            @ParameterName("rows") Integer rows,
-            @ParameterName("reportEvery") Integer reportEvery,
+            @ParameterName("rows") String sRows,
+            @ParameterName("reportEvery") String sReportEvery,
             @ParameterName("onlyShowConfiguration") Boolean onlyShowConfiguration
     ) throws Exception {
 
@@ -29,8 +29,8 @@ public class CDKDeleteLibraryProcess {
                 destinationUrl,
                 library,
                 filterQuery,
-                rows != null ? rows : 300,
-                reportEvery != null ? reportEvery : 5000,
+                sRows != null ? Integer.valueOf(sRows) : 300,
+                sReportEvery != null ? Integer.valueOf(sReportEvery) : 5000,
                 Boolean.TRUE.equals(onlyShowConfiguration));
     }
 
@@ -45,8 +45,8 @@ public class CDKDeleteLibraryProcess {
         String library = "tul";
         //String filterQuery = "pid:\"uuid:77274343-b156-4287-99ae-3a2d25e20ee7\"";
         String filterQuery = "";
-        Integer rows = 20;
-        Integer reportEvery = 5000;
+        String rows = "20";
+        String reportEvery = "5000";
         Boolean onlyShowConfiguration = false;
 
         CDKDeleteLibraryProcess.deleteLibraryMain(
@@ -63,8 +63,8 @@ public class CDKDeleteLibraryProcess {
         String library = "kkkv";
         //String filterQuery = "pid:\"uuid:31121f1c-649f-4deb-8e9e-501ed5780520\"";
         String filterQuery = "";
-        Integer rows = 20;
-        Integer reportEvery = 5000;
+        String rows = "20";
+        String reportEvery = "5000";
         Boolean onlyShowConfiguration = false;
 
         CDKDeleteLibraryProcess.deleteLibraryMain(
